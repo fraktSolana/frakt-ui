1f217dd8a13a4868cda9edd935af1e63fb6e1175
sablevsky <vladislav1145@gmail.com>
2021-10-12 19:00:31 +0400

    Fix container. Start on Home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,17 @@
+import { Container } from '../../components/Layout';
 import { AppLayout } from '../../components/Layout/AppLayout';
+import styles from './styles.module.scss';
 
 const HomePage = (): JSX.Element => {
   return (
     <AppLayout>
-      <p>Home page</p>
+      <Container component="main" className={styles.container}>
+        <h1 className={styles.title}>
+          BUY, SELL AND <b>FRAKTIONALIZE</b> NFTs
+        </h1>
+        <h2 className={styles.subtitle}>Launching soon on Solana</h2>
+        <div className={styles.separator} />
+      </Container>
     </AppLayout>
   );
 };
