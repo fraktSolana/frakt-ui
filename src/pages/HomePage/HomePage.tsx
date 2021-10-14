4fdfd8a371e3159f5149984109ca4127f11dabec
sablevsky <vladislav1145@gmail.com>
2021-10-14 18:52:17 +0400

    Add helmet title on Home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet';
 import { Container } from '../../components/Layout';
 import { AppLayout } from '../../components/Layout/AppLayout';
 import KeyPointsSection from './sections';
@@ -6,6 +7,9 @@ import styles from './styles.module.scss';
 const HomePage = (): JSX.Element => {
   return (
     <AppLayout>
+      <Helmet>
+        <title>FRAKTION ART</title>
+      </Helmet>
       <Container component="main" className={styles.container}>
         <h1 className={styles.title}>
           BUY, SELL AND <b>FRAKTIONALIZE</b> NFTs
