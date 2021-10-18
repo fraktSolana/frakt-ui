357b8db407548c63bb71ee031f8213003932fd72
Kirill Dryga <webpkt@gmail.com>
2021-10-18 17:49:12 +0300

    fix button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,7 @@
 import styles from './styles.module.scss';
 import classNames from 'classnames/bind';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface IButtonRegularProps {
   className?: string;
@@ -10,14 +12,15 @@ interface IButtonRegularProps {
   size?: string;
   Icon?: any;
   children: any;
-  type?: 'primary' | 'secondary';
+  type?: 'primary' | 'secondary' | 'alternative';
+  href?: string;
 }
 
 const getIcon = ({ Icon }) => {
   return Icon ? <Icon size={14} /> : '';
 };
 
-const ButtonRegular = ({
+const Button = ({
   className,
   disabled = false,
   isLink = false,
@@ -30,15 +33,20 @@ const ButtonRegular = ({
 }: IButtonRegularProps): JSX.Element => {
   if (isLink) {
     return (
-      <a
-        className={`${styles.root} ${className || ''} ${
-          disabled ? styles.disabled : ''
-        } ${styles[size]}`}
+      <Link
+        className={classNames([
+          styles.root,
+          styles.link,
+          className,
+          { [styles.disabled]: disabled },
+          styles[size],
+          styles[type],
+        ])}
         {...linkAttrs}
       >
         {getIcon({ Icon })}
         {children}
-      </a>
+      </Link>
     );
   }
 
@@ -60,4 +68,4 @@ const ButtonRegular = ({
   );
 };
 
-export default ButtonRegular;
+export default Button;
