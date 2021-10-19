ce9e863199e85e73fb1bf9ddcaf480b1ad915be8
sablevsky <vladislav1145@gmail.com>
2021-10-19 16:53:00 +0300

    Refactor Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,7 @@
-import styles from './styles.module.scss';
-import classNames from 'classnames/bind';
-import React from 'react';
 import { Link } from 'react-router-dom';
+import classNames from 'classnames/bind';
+
+import styles from './styles.module.scss';
 
 interface IButtonRegularProps {
   className?: string;
@@ -9,16 +9,9 @@ interface IButtonRegularProps {
   isLink?: boolean;
   linkAttrs?: any;
   onClick?: (args: any) => any;
-  size?: string;
-  Icon?: any;
   children: any;
   type?: 'primary' | 'secondary' | 'alternative';
-  href?: string;
 }
-
-const getIcon = ({ Icon }) => {
-  return Icon ? <Icon size={14} /> : '';
-};
 
 const Button = ({
   className,
@@ -26,9 +19,7 @@ const Button = ({
   isLink = false,
   linkAttrs,
   onClick = () => {},
-  size = 'md',
   type = 'primary',
-  Icon,
   children,
 }: IButtonRegularProps): JSX.Element => {
   if (isLink) {
@@ -39,12 +30,10 @@ const Button = ({
           styles.link,
           className,
           { [styles.disabled]: disabled },
-          styles[size],
           styles[type],
         ])}
         {...linkAttrs}
       >
-        {getIcon({ Icon })}
         {children}
       </Link>
     );
@@ -57,12 +46,10 @@ const Button = ({
         styles.root,
         className,
         { [styles.disabled]: disabled },
-        styles[size],
         styles[type],
       ])}
       onClick={onClick}
     >
-      {getIcon({ Icon })}
       {children}
     </button>
   );
