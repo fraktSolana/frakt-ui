85cb466b5c2be2547b54b963a2e2a16da99f9a83
sablevsky <vladislav1145@gmail.com>
2021-10-20 16:27:26 +0300

    Implement some changes

--- src/pages/VaultsPage/VaultsPage.tsx
+++ src/pages/VaultsPage/VaultsPage.tsx
@@ -1,9 +1,22 @@
+import VaultCard, {
+  MOCK_PROPS as VaultCardMockProps,
+} from '../../components/VaultCard';
+import { Container } from '../../components/Layout';
 import { AppLayout } from '../../components/Layout/AppLayout';
+import styles from './styles.module.scss';
 
 const VaultsPage = (): JSX.Element => {
+  const cardsInfo = Array(100).fill(VaultCardMockProps);
+
   return (
     <AppLayout>
-      <p>Vaults page</p>
+      <Container component="main" className={styles.content}>
+        <div className={styles.cards}>
+          {cardsInfo.map((props, idx) => (
+            <VaultCard key={idx} {...props} />
+          ))}
+        </div>
+      </Container>
     </AppLayout>
   );
 };
