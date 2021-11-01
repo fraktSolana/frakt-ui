cab146f146533d93f8d4b94de41cc79b1082b8d8
sablevsky <vladislav1145@gmail.com>
2021-11-01 14:59:28 +0300

    Remove unused MarketProvider

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { WalletProvider } from './external/contexts/wallet';
 import { ConnectionProvider } from './external/contexts/connection';
 import { AccountsProvider } from './external/contexts/accounts';
-import { MarketProvider } from './external/contexts/market';
 import { URLS } from './constants';
 
 import Page404 from './pages/Page404';
@@ -20,47 +18,45 @@ export function Routes(): JSX.Element {
       <ConnectionProvider>
         <WalletProvider>
           <AccountsProvider>
-            <MarketProvider>
-              <Switch>
-                <Route
-                  exact
-                  path={URLS.ROOT}
-                  component={(): JSX.Element => <HomePage />}
-                />
-                <Route
-                  exact
-                  path={URLS.VAULTS}
-                  component={(): JSX.Element => <VaultsPage />}
-                />
-                <Route
-                  exact
-                  path={URLS.EXCHANGE}
-                  component={(): JSX.Element => <ExchangePage />}
-                />
-                <Route
-                  exact
-                  path={URLS.STAKER_PAGE}
-                  component={(): JSX.Element => <StakerPage />}
-                />
-                <Route
-                  exact
-                  path={URLS.FRAKTIONALIZE}
-                  component={(): JSX.Element => <FraktionalizePage />}
-                />
+            <Switch>
+              <Route
+                exact
+                path={URLS.ROOT}
+                component={(): JSX.Element => <HomePage />}
+              />
+              <Route
+                exact
+                path={URLS.VAULTS}
+                component={(): JSX.Element => <VaultsPage />}
+              />
+              <Route
+                exact
+                path={URLS.EXCHANGE}
+                component={(): JSX.Element => <ExchangePage />}
+              />
+              <Route
+                exact
+                path={URLS.STAKER_PAGE}
+                component={(): JSX.Element => <StakerPage />}
+              />
+              <Route
+                exact
+                path={URLS.FRAKTIONALIZE}
+                component={(): JSX.Element => <FraktionalizePage />}
+              />
 
-                <Route
-                  exact
-                  path={URLS.PAGE_404}
-                  component={(): JSX.Element => <Page404 />}
-                />
-                <Route
-                  exact
-                  path={'*'}
-                  component={(): JSX.Element => <Page404 />}
-                />
-              </Switch>
-              <ConnectWalletModal />
-            </MarketProvider>
+              <Route
+                exact
+                path={URLS.PAGE_404}
+                component={(): JSX.Element => <Page404 />}
+              />
+              <Route
+                exact
+                path={'*'}
+                component={(): JSX.Element => <Page404 />}
+              />
+            </Switch>
+            <ConnectWalletModal />
           </AccountsProvider>
         </WalletProvider>
       </ConnectionProvider>
