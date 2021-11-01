34246c718c1efe8ed3b7088477360c41cdaa464f
sablevsky <vladislav1145@gmail.com>
2021-11-02 00:01:22 +0300

    Add our lib deps. Change fraktionalize page a bit

--- src/pages/FraktionalizePage/FraktionalizePage.tsx
+++ src/pages/FraktionalizePage/FraktionalizePage.tsx
@@ -1,9 +1,34 @@
+import { Container } from '../../components/Layout';
 import { AppLayout } from '../../components/Layout/AppLayout';
+import styles from './styles.module.scss';
+
+// const useUserTokens = () => {
+//   const { wallet, connected } = useWallet();
+//   const connection = useConnection();
+//   const [tokens, setTokens] = useState([]);
+//   const [loading, setLoading] = useState<boolean>(false);
+
+//   const fetchTokens = async () => {
+//     setLoading(true);
+//     const tokens = await getAllUserTokens(wallet.publicKey, { connection });
+
+//     const a = await connection.getParsedProgramAccounts(
+//       new PublicKey('D1bz9T4br5DaRfYx48aSaQsstcfuaytvvArxVjShLwca'),
+//     );
+//     console.log(a);
+//   };
+
+//   useEffect(() => {
+//     connected && fetchTokens();
+//   }, [connected]);
+// };
 
 const FraktionalizePage = (): JSX.Element => {
   return (
     <AppLayout>
-      <p>Fraktionalize page</p>
+      <Container component="main" className={styles.content}>
+        <p>Fraktionalize page</p>
+      </Container>
     </AppLayout>
   );
 };
