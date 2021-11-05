8807f237235635b452151bdd302784889b6c1790
Kirill Dryga <webpkt@gmail.com>
2021-11-05 14:24:31 +0300

    add input restrictions

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,13 +3,40 @@ import styles from './styles.module.scss';
 import { Input as InputAnt, InputProps as InputPropsAnt } from 'antd';
 import classNames from 'classnames';
 
+interface InputProps extends InputPropsAnt {
+  disableSymbols?: boolean;
+  disableNumbers?: boolean;
+  customRegexp?: string;
+}
+
 export const Input = React.forwardRef(
-  ({ className, ...props }: InputPropsAnt, ref) => {
+  (
+    {
+      className,
+      disableSymbols = false,
+      disableNumbers = false,
+      customRegexp,
+      ...props
+    }: InputProps,
+    ref,
+  ) => {
+    const onChange = (e) => {
+      if (!props.onChange) return;
+      let value = e.target.value;
+      if (disableNumbers) value = value.replaceAll(/[0-9]/g, '');
+      if (disableSymbols) value = value.replaceAll(/[^a-zA-Z0-9 ]/g, '');
+      if (customRegexp) value = value.replaceAll(new RegExp(customRegexp), '');
+
+      e.target.value = value;
+      props.onChange(e);
+    };
+
     return (
       <InputAnt
         className={classNames(styles.input, className)}
         ref={ref as LegacyRef<InputAnt>}
         {...props}
+        onChange={onChange}
       />
     );
   },
