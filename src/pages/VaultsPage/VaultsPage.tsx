ac7235c2ee6b8fe7cf2547ef52e98e37378ec50e
sablevsky <vladislav1145@gmail.com>
2021-11-09 01:13:13 +0300

    Implement filter

--- src/pages/VaultsPage/VaultsPage.tsx
+++ src/pages/VaultsPage/VaultsPage.tsx
@@ -12,23 +12,36 @@ import { useDebounce } from '../../hooks';
 import { useFraktion } from '../../contexts/fraktion/fraktion.context';
 import { NavLink } from 'react-router-dom';
 import { URLS } from '../../constants';
+import Toggle from '../../components/Toggle';
+import classNames from 'classnames/bind';
+import { VaultState } from '../../contexts/fraktion/fraktion.model';
 
 const VaultsPage = (): JSX.Element => {
   const { loading, vaults: rawVaults } = useFraktion();
   const [searchString, setSearchString] = useState<string>('');
   const { itemsToShow, next } = useFakeInfinityScroll(9);
+  const [filterActiveVaults, setFilterActiveVaults] = useState<boolean>(false);
+  const [filterBoughtVaults, setFilterBoughtVaults] = useState<boolean>(false);
+  const [filterClosedVaults, setFilterClosedVaults] = useState<boolean>(false);
 
   const searchItems = useDebounce((search: string) => {
     setSearchString(search.toUpperCase());
   }, 300);
 
   const vaults = useMemo(() => {
-    return rawVaults.filter(({ name }) =>
-      name.toUpperCase().includes(searchString),
-    );
+    return rawVaults
+      .filter(({ state }) => !(filterActiveVaults && state === VaultState[1]))
+      .filter(({ state }) => !(filterBoughtVaults && state === VaultState[2]))
+      .filter(({ state }) => !(filterClosedVaults && state === VaultState[3]))
+      .filter(({ name }) => name.toUpperCase().includes(searchString));
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchString, rawVaults]);
-
+  }, [
+    searchString,
+    rawVaults,
+    filterActiveVaults,
+    filterBoughtVaults,
+    filterClosedVaults,
+  ]);
   return (
     <AppLayout>
       <Container component="main" className={styles.content}>
@@ -38,6 +51,24 @@ const VaultsPage = (): JSX.Element => {
           className={styles.search}
           placeholder="Search by vault name"
         />
+        <div className={styles.filters}>
+          <FilterToggle
+            checked={!filterActiveVaults}
+            toggleChecked={() => setFilterActiveVaults((prev) => !prev)}
+            name="Active Vaults"
+          />
+          <FilterToggle
+            checked={!filterBoughtVaults}
+            toggleChecked={() => setFilterBoughtVaults((prev) => !prev)}
+            name="Bought Vaults"
+          />
+          <FilterToggle
+            checked={!filterClosedVaults}
+            toggleChecked={() => setFilterClosedVaults((prev) => !prev)}
+            name="Closed Vaults"
+          />
+        </div>
+
         <FakeInfinityScroll
           itemsToShow={itemsToShow}
           next={next}
@@ -72,6 +103,30 @@ const VaultsPage = (): JSX.Element => {
         </FakeInfinityScroll>
       </Container>
     </AppLayout>
+  );
+};
+
+const FilterToggle = ({
+  checked,
+  toggleChecked = () => {},
+  name,
+}: {
+  checked?: boolean;
+  toggleChecked?: () => void;
+  name: string;
+}): JSX.Element => {
+  return (
+    <div className={styles.filterToggle} onClick={toggleChecked}>
+      <Toggle checked={!!checked} />
+      <p
+        className={classNames([
+          styles.filterToggle__text,
+          { [styles.filterToggle__text_muted]: !checked },
+        ])}
+      >
+        {name}
+      </p>
+    </div>
   );
 };
 
