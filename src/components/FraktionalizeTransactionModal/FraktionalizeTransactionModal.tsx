51eb03f077284054bf7bb0a981f5a5c28f9caa63
Beardev118 <otoku118@gmail.com>
2021-11-23 20:55:23 +0400

    add market creation transactions from serum

--- src/components/FraktionalizeTransactionModal/FraktionalizeTransactionModal.tsx
+++ src/components/FraktionalizeTransactionModal/FraktionalizeTransactionModal.tsx
@@ -1,7 +1,14 @@
+import { PublicKey } from '@solana/web3.js';
+import { MARKETS } from '@project-serum/serum';
+import { useConnection } from '../../external/contexts/connection';
+import { useWallet } from '../../external/contexts/wallet';
+import { notify } from '../../external/utils/notifications';
+import { listMarket } from '../../utils/serum-utils/send';
 import Button from '../Button';
 import { Loader } from '../Loader';
 import { Modal } from '../Modal';
 import styles from './styles.module.scss';
+import { useEffect, useState } from 'react';
 
 interface FraktionalizeTransactionModalProps {
   state?: 'loading' | 'success' | 'fail';
@@ -18,6 +25,8 @@ const FraktionalizeTransactionModal = ({
   onCancel,
   onRetryClick = () => {},
 }: FraktionalizeTransactionModalProps): JSX.Element => {
+  const [creatingMarket, setCreatingMarket] = useState(false);
+  const [listedMarket, setListedMarket] = useState('');
   const loadingContent = (
     <div className={styles.loadingContent}>
       <Loader size="large" />
@@ -25,11 +34,49 @@ const FraktionalizeTransactionModal = ({
     </div>
   );
 
+  const connection = useConnection();
+  const { wallet } = useWallet();
+  const dexProgramId = MARKETS.find(({ deprecated }) => !deprecated).programId;
+  const lotSize = 0.1;
+  const tickSize = 0.00001;
+  const baseTokenDecimals = 3; // FRAKTION_DECIMALS
+  const quoteTokenDecimals = 8; // SOL DECIMALS
+  const baseLotSize = Math.round(10 ** baseTokenDecimals * lotSize);
+  const quoteLotSize = Math.round(
+    lotSize * 10 ** quoteTokenDecimals * tickSize,
+  );
+
+  const createFraktionsMarket = async (fractionsMintAddress) => {
+    setCreatingMarket(true);
+    try {
+      const marketAddress = await listMarket({
+        connection,
+        wallet,
+        baseMint: new PublicKey(fractionsMintAddress),
+        quoteMint: new PublicKey('So11111111111111111111111111111111111111112'),
+        baseLotSize,
+        quoteLotSize,
+        dexProgramId,
+      });
+      setListedMarket(marketAddress.toBase58());
+      //
+    } catch (e) {
+      console.warn(e);
+      notify({
+        message: 'Error listing new market',
+        description: e.message,
+        type: 'error',
+      });
+    } finally {
+      setCreatingMarket(false);
+    }
+  };
+
   const successContent = (
     <div className={styles.successContent}>
       <h2 className={styles.successContent__title}>Congratulations!</h2>
       <p className={styles.successContent__subtitle}>
-        Now feel free to do anything with your fraktions!
+        Tokens of your NFT should be in your wallet!
       </p>
       {!!fractionsMintAddress && (
         <div className={styles.successContent__fractionsMintWrapper}>
@@ -39,7 +86,12 @@ const FraktionalizeTransactionModal = ({
           <b>{fractionsMintAddress}</b>
         </div>
       )}
-      {/* <button>Create liquidity pool on Raydium</button> //TODO: implement when ready */}
+      <p className={styles.successContent__subtitle}>
+        If you want fraktions to be tradable you need to create market
+      </p>
+      <button onClick={() => createFraktionsMarket(fractionsMintAddress)}>
+        Create Market
+      </button>
     </div>
   );
 
