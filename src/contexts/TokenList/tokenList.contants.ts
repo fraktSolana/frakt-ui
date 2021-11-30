ca33dd81181b7a5a7061cd4ff7ae742ba1c541a9
sablevsky <vladislav1145@gmail.com>
2021-11-30 20:49:40 +0300

    Remove RAY pool

--- src/contexts/TokenList/tokenList.contants.ts
+++ src/contexts/TokenList/tokenList.contants.ts
@@ -6,5 +6,5 @@ export const VERIFIED_BY_FRAKT_TEAM_TOKENS_URL =
 //? Add some external spl tokens in swappableTokensMap
 export const ADDITIONAL_SWAPPABLE_TOKENS_MINTS = [
   FRKT_TOKEN_MINT_PUBLIC_KEY,
-  '4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R', //? RAY
+  // '4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R', //? RAY
 ];
