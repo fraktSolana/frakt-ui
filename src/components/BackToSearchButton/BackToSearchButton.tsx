2901ceec83514ef0cfbcab467af400a2f0c94f22
Roman Dogoda <piterom911@gmail.com>
2021-12-13 16:45:15 +0200

    Create BackToSearchButton

---
+++ src/components/BackToSearchButton/BackToSearchButton.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './styles.module.scss';
+import { NavLink } from 'react-router-dom';
+import { URLS } from '../../constants';
+import { ArrowLeftIcon } from '../../icons';
+import classNames from 'classnames';
+
+interface BackToSearchButtonProps {
+  className?: string;
+}
+
+export const BackToSearchButton = ({
+  className,
+}: BackToSearchButtonProps): JSX.Element => {
+  return (
+    <div className={classNames([className, styles.buttonWrapper])}>
+      <NavLink className={styles.backLink} to={URLS.VAULTS}>
+        <ArrowLeftIcon className={styles.arrowIcon} />
+        Back to Vaults list
+      </NavLink>
+    </div>
+  );
+};
---
+++ src/components/BackToSearchButton/index.ts
@@ -0,0 +1,1 @@
+export { BackToSearchButton } from './BackToSearchButton';
