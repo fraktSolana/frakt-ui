82555f489787382e57a1ba384d42baa36ba436ab
Piterom911 <58930444+Piterom911@users.noreply.github.com>
2021-12-17 16:31:59 +0200

    Create AuctionCountdown component
    * Create auction countdown component
    
    * Fix PR comments
    
    * Fix PR comments — naming and types

---
+++ src/components/AuctionCountdown/AuctionCountdown.tsx
@@ -0,0 +1,54 @@
+import moment from 'moment';
+import React, { useEffect, useRef, useState } from 'react';
+import styles from './styles.module.scss';
+import classNames from 'classnames';
+
+interface AuctionCountdownProps {
+  endTime: number;
+  className?: string;
+}
+
+export const AuctionCountdown = ({
+  endTime,
+  className,
+}: AuctionCountdownProps): JSX.Element => {
+  const endTimeMoment = moment.unix(endTime);
+
+  const intervalIdRef = useRef<ReturnType<typeof setInterval>>(null);
+
+  const [currentTime, setCurrentTime] = useState<moment.Moment>(moment());
+  const timeDifference = moment.duration(endTimeMoment.diff(currentTime));
+
+  const formatDateUnit = (value: number): string => {
+    return value < 10 ? `0${value}` : `${value}`;
+  };
+
+  useEffect(() => {
+    intervalIdRef.current = setInterval(() => {
+      setCurrentTime(moment());
+    }, 1000);
+
+    return () => clearInterval(intervalIdRef.current);
+  }, []);
+
+  useEffect(() => {
+    timeDifference.asSeconds() < 0 && clearInterval(intervalIdRef.current);
+  }, [timeDifference]);
+
+  return (
+    <ul className={classNames(className, styles.countdown)}>
+      <li className={styles.timeItem}>
+        {formatDateUnit(timeDifference.hours() + timeDifference.days() * 24)}
+        <span>Hours</span>
+      </li>
+      <li className={styles.timeItem}>
+        {formatDateUnit(timeDifference.minutes())}
+        <span>Minutes</span>
+      </li>
+      <li className={styles.timeItem}>
+        {formatDateUnit(timeDifference.seconds())}
+        <span>Seconds</span>
+      </li>
+    </ul>
+  );
+};
