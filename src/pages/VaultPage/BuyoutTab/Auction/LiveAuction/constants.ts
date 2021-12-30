bdcbd6fb7bc15adde7a0234ea80d79c961374f69
sablevsky <vladislav1145@gmail.com>
2021-12-30 16:22:14 +0300

    Fix ending page duration for production

--- src/pages/VaultPage/BuyoutTab/Auction/LiveAuction/constants.ts
+++ src/pages/VaultPage/BuyoutTab/Auction/LiveAuction/constants.ts
@@ -1,3 +1,3 @@
 const IS_DEV = process.env.REACT_APP_NETWORK === 'devnet';
 
-export const ENDING_PHASE_DURATION = IS_DEV ? 10 * 60 : 1 * 60 * 60; //? Duration in seconds
+export const ENDING_PHASE_DURATION = IS_DEV ? 10 * 60 : 24 * 60 * 60; //? Duration in seconds
