4d9a510f99e47bf38a1f58d70c9c81462bb47af7
Roman Dogoda <piterom911@gmail.com>
2022-01-07 11:44:58 +0200

    Create sidebar Slider component

--- src/pages/FraktionalizePage/Sidebar/Header/Header.tsx
+++ src/pages/FraktionalizePage/Sidebar/Header/Header.tsx
@@ -1,14 +1,7 @@
 import { UserNFT } from '../../../../contexts/userTokens';
 import styles from './styles.module.scss';
 import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
-import 'swiper/swiper.min.css';
-import 'swiper/modules/navigation/navigation.scss';
-import 'swiper/modules/pagination/pagination.scss';
-import 'swiper/modules/thumbs/thumbs';
-import SwiperCore, { Navigation, Scrollbar } from 'swiper';
-
-SwiperCore.use([Navigation, Scrollbar]);
+import { Slider } from './Slider';
 
 interface HeaderProps {
   nfts: UserNFT[];
@@ -20,12 +13,6 @@ interface HeaderProps {
   }[];
 }
 
-const sliderBreakpoints = {
-  250: { slidesPerView: 3 },
-  320: { slidesPerView: 3.6 },
-  1450: { slidesPerView: 4 },
-};
-
 export const Header = ({
   nfts,
   onDeselect,
@@ -35,33 +22,7 @@ export const Header = ({
   return (
     <div className={styles.header}>
       <p className={styles.title}>{isBasket ? 'Your NFTs' : 'Your NFT'}</p>
-      <Swiper
-        className={styles.nftSlider}
-        spaceBetween={18}
-        breakpoints={sliderBreakpoints}
-        navigation={true}
-        scrollbar={{ draggable: true }}
-      >
-        {lockedNFT.map(({ nftImage, nftMint }) => (
-          <SwiperSlide
-            key={nftMint}
-            className={styles.image}
-            style={{ backgroundImage: `url(${nftImage})` }}
-          />
-        ))}
-        {nfts.map((nft, idx) => (
-          <SwiperSlide
-            key={idx}
-            className={styles.image}
-            style={{ backgroundImage: `url(${nft?.metadata?.image})` }}
-          >
-            <button
-              className={styles.removeBtn}
-              onClick={() => onDeselect(nft)}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <Slider nfts={nfts} lockedNFT={lockedNFT} onDeselect={onDeselect} />
     </div>
   );
 };
