40a0472494ec1540f4b8a96251cdfd734c35f53e
sablevsky <vladislav1145@gmail.com>
2022-02-17 18:25:49 +0300

    Remove unused useEffect

--- src/contexts/nftPools/nftPools.context.tsx
+++ src/contexts/nftPools/nftPools.context.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 
 import { usePolling } from '../../hooks';
