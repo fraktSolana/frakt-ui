b5497e17bb989d6e9f105aab08d6170c406ac828
Roman Dogoda <piterom911@gmail.com>
2022-02-19 21:34:37 +0200

    Create percent block

--- src/pages/HomePage/sections/FullPotentialSection/YieldBlock/YieldBlock.tsx
+++ src/pages/HomePage/sections/FullPotentialSection/YieldBlock/YieldBlock.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from 'react';
 import classNames from 'classnames';
 import styles from './styles.module.scss';
-import { YieldInfoIcon } from '../../../../../icons';
+import {
+  PercentIcon100,
+  PercentIcon30,
+  PercentIcon50,
+  YieldInfoIcon,
+} from '../../../../../icons';
 import { BlockContent } from '../BlockContent';
 
 interface YieldBlockProps {
@@ -18,6 +23,11 @@ export const YieldBlock: FC<YieldBlockProps> = ({ className }) => {
           'Provide NFTs or liquidity to protocol and to the NFT pools & vaults and reap the rewards'
         }
       />
+      <div className={styles.percentsWrapper}>
+        <PercentIcon100 className={styles.percent100} />
+        <PercentIcon50 className={styles.percent50} />
+        <PercentIcon30 className={styles.percent30} />
+      </div>
     </div>
   );
 };
