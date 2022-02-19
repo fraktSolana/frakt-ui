84f1268fce7787726eea7ef7fc3be651d1f1a133
Roman Dogoda <piterom911@gmail.com>
2022-02-19 17:17:00 +0200

    Fix pools block slider

--- src/pages/HomePage/sections/FullPotentialSection/PoolsBlock/PoolsBlock.tsx
+++ src/pages/HomePage/sections/FullPotentialSection/PoolsBlock/PoolsBlock.tsx
@@ -1,14 +1,21 @@
-import React, { FC } from 'react';
+import React, { FC, useRef } from 'react';
 import classNames from 'classnames';
 import styles from './styles.module.scss';
 import { PoolsInfoIcon } from '../../../../../icons';
 import { BlockContent } from '../BlockContent';
+import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
+import SwiperCore, { Navigation, Autoplay, Scrollbar } from 'swiper';
+import { PoolCard } from './PoolCard';
+
+SwiperCore.use([Navigation, Autoplay, Scrollbar]);
 
 interface PoolsBlockProps {
   className?: string;
 }
 
 export const PoolsBlock: FC<PoolsBlockProps> = ({ className }) => {
+  const prevBtn = useRef<HTMLDivElement>(null);
+  const nextBtn = useRef<HTMLDivElement>(null);
   return (
     <div className={classNames(className, styles.block)}>
       <BlockContent
@@ -16,6 +23,42 @@ export const PoolsBlock: FC<PoolsBlockProps> = ({ className }) => {
         icon={<PoolsInfoIcon />}
         text={'Instantly buy, sell and swap NFTs'}
       />
+      <div className={styles.sliderWrapper}>
+        <Swiper
+          slidesPerView={2.5}
+          className={styles.slider}
+          navigation={{
+            prevEl: prevBtn.current,
+            nextEl: nextBtn.current,
+          }}
+          loop={true}
+          loopAdditionalSlides={1}
+          spaceBetween={0}
+          speed={1000}
+          autoplay={{ delay: 5000, disableOnInteraction: false }}
+        >
+          <SwiperSlide>
+            <PoolCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <PoolCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <PoolCard />
+          </SwiperSlide>
+          <SwiperSlide>
+            <PoolCard />
+          </SwiperSlide>
+        </Swiper>
+        <div
+          ref={prevBtn}
+          className={`${styles.sliderNavPrev} sliderNavPrev`}
+        />
+        <div
+          ref={nextBtn}
+          className={`${styles.sliderNavNext} sliderNavNext`}
+        />
+      </div>
     </div>
   );
 };
