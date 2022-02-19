73b00ab46bad570d42de5c9cdd6d217573e657a5
Roman Dogoda <piterom911@gmail.com>
2022-02-19 20:25:18 +0200

    Add Image block to Lending & Borrowing card

--- src/pages/HomePage/sections/FullPotentialSection/LendingBlock/LendingBlock.tsx
+++ src/pages/HomePage/sections/FullPotentialSection/LendingBlock/LendingBlock.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from 'react';
 import classNames from 'classnames';
 import styles from './styles.module.scss';
-import { LandingInfoIcon } from '../../../../../icons';
+import { LandingInfoIcon, SolanaIcon } from '../../../../../icons';
 import { BlockContent } from '../BlockContent';
+import imageOne from '../../../images/landing1.png';
+import imageTwo from '../../../images/landing2.png';
 
 interface LendingBlockProps {
   className?: string;
@@ -11,6 +13,22 @@ interface LendingBlockProps {
 export const LendingBlock: FC<LendingBlockProps> = ({ className }) => {
   return (
     <div className={classNames(className, styles.block)}>
+      <div className={styles.imagesWrapper}>
+        <div className={classNames(styles.image, styles.imageOne)}>
+          <img src={imageOne} alt="" />
+        </div>
+        <div className={classNames(styles.image, styles.imageTwo)}>
+          <img src={imageTwo} alt="" />
+          <div className={styles.infoWrapper}>
+            <p className={styles.infoTitle}>TYR-1281</p>
+            <p className={styles.infoPrice}>
+              100
+              <SolanaIcon />
+              SOL
+            </p>
+          </div>
+        </div>
+      </div>
       <BlockContent
         title={'Lending & Borrowing'}
         icon={<LandingInfoIcon />}
