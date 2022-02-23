1700a20006f43a248dd2c569de4974b4f2727d8f
Roman Dogoda <piterom911@gmail.com>
2022-02-23 11:19:37 +0200

    create poolsData file

---
+++ src/pages/HomePage/sections/FullPotentialSection/PoolsBlock/poolsData.ts
@@ -0,0 +1,27 @@
+import {
+  CommunityPoolState,
+  NftPoolData,
+} from '../../../../../utils/cacher/nftPools';
+import { PublicKey } from '@solana/web3.js';
+import BN from 'bn.js';
+
+// export const POOLS_DATA: NftPoolData = {
+//   publicKey: {
+//     _bn: new BN(1),
+//     words: (11) [51545689, 47817706, 10399790, 15078405, 37845856, 20643022, 19572232, 4027837, 21590092, 2628231, 0],
+//   },
+//   authority: "",
+//   createdAt: new BN(1),
+//   fractionMint: "",
+//   fractionsSupply: new BN(1),
+//   state: "",
+//   tokenProgram: "",
+//   tokenTypeCount: "",
+//   lotteryTickets: [
+//     {
+//       publicKey: "",
+//     }
+//   ],
+//   poolWhitelist: [],
+//   safetyBoxes: [],
+// }
