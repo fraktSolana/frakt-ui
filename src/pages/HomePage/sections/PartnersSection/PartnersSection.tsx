cc9c5e19d2b67615d95b465fdfdd4f04eeb647aa
sablevsky <vladislav1145@gmail.com>
2022-03-03 19:00:13 +0300

    Fix backers section

--- src/pages/HomePage/sections/PartnersSection/PartnersSection.tsx
+++ src/pages/HomePage/sections/PartnersSection/PartnersSection.tsx
@@ -1,42 +1,33 @@
-import React, { FC } from 'react';
-import classNames from 'classnames/bind';
-import styles from './styles.module.scss';
+import { FC } from 'react';
+
+import { TECHNICAL_PARTNERS_ID } from '../../constants';
+import styles from './PartnersSection.module.scss';
 import exchange from './images/exchange.svg';
 import digitalEyes from './images/digitalEyes.svg';
 import magicEden from './images/magicEden.svg';
 import radium from './images/radium.svg';
 import serum from './images/serum.svg';
 import solsea from './images/solsea.svg';
-import { TECHNICAL_PARTNERS_ID } from '../../constants';
+import { Container } from '../../../../components/Layout';
 
 interface PartnersSectionProps {
-  className?: string;
   navRef?: { current: HTMLParagraphElement };
 }
 
-const PartnersSection: FC<PartnersSectionProps> = ({ className, navRef }) => {
+const PartnersSection: FC<PartnersSectionProps> = ({ navRef }) => {
   return (
-    <section className={classNames(styles.section, className)}>
-      <p
-        className="itemForIntersectionMenu"
-        id={TECHNICAL_PARTNERS_ID}
-        ref={navRef}
-      >
+    <Container component="section" className={styles.root}>
+      <h3 className={styles.title} ref={navRef} id={TECHNICAL_PARTNERS_ID}>
         Backers
-      </p>
-      <div className={`container`}>
-        <h2 className={styles.title}>Technical Partners</h2>
-        <p className={styles.subtitle}>Technical Partners</p>
-        <div className={styles.brands}>
-          <img src={radium} alt="Brand logo" />
-          <img src={serum} alt="Brand logo" />
-          <img src={exchange} alt="Brand logo" />
-          <img src={digitalEyes} alt="Brand logo" />
-          <img src={magicEden} alt="Brand logo" />
-          <img src={solsea} alt="Brand logo" />
-        </div>
+      </h3>
+      <div className={styles.brands}>
+        {[radium, serum, exchange, digitalEyes, magicEden, solsea].map(
+          (imageUrl, idx) => (
+            <img key={idx} src={imageUrl} alt="Brand logo" />
+          ),
+        )}
       </div>
-    </section>
+    </Container>
   );
 };
 
