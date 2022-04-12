ef1d62f84dc1214c4987775410498bc3cc28b1b8
sablevsky <vladislav1145@gmail.com>
2022-04-12 13:05:13 +0400

    Hide pools in development

--- src/utils/cacher/nftPools/nftPools.constants.ts
+++ src/utils/cacher/nftPools/nftPools.constants.ts
@@ -1,1 +1,6 @@
-export const HIDDEN_POOLS = [];
+export const HIDDEN_POOLS = [
+  'CA5qBCJS5wwdvMmt2ANLUxUecmNjf7Py9s7N1UEJ1cxy',
+  'AaYBdEnjqYoay3ozs2tY1tm9kFXxjUrAZeNZngnqwmV1',
+  'HfEJXp9YNdrrPuqAoaeKzL3pyCvsdnVQxHXpf7KVTsjY',
+  '7Bjj5LSjvC6Sumvx3L3gK25AGVGhFfPyZWkm1xep8kXv',
+];
