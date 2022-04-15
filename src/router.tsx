095c2616ff0acc03da838760130ea8d11979d337
sablevsky <vladislav1145@gmail.com>
2022-04-16 00:25:50 +0400

    Add redirect for nft pools

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,17 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
+import { POOLS, POOL_TABS } from './constants';
 import { routes } from './constants/routes';
 
 export const Router = (): JSX.Element => {
   return (
     <BrowserRouter>
       <Switch>
+        <Redirect
+          from={`${POOLS}/:poolPubkey`}
+          to={`${POOLS}/:poolPubkey/${POOL_TABS.BUY}`}
+          exact
+        />
         {routes.map(({ exact, path, component: Component }, index) => (
           <Route
             key={index}
