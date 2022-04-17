cd0ced0f284c485e4eaf61342029909a3e53f70d
sablevsky <vladislav1145@gmail.com>
2022-04-17 21:54:14 +0400

    Create thugbirdz pool

--- src/utils/cacher/nftPools/nftPools.constants.ts
+++ src/utils/cacher/nftPools/nftPools.constants.ts
@@ -1,5 +1,5 @@
 export const HIDDEN_POOLS = [
-  '3da5KmaRp9gHx2wzv8PdZHpxXWvbHMRUxCK9FCLvHGZr', //? thugbirds
+  // '3da5KmaRp9gHx2wzv8PdZHpxXWvbHMRUxCK9FCLvHGZr', //? thugbirds
   'CA5qBCJS5wwdvMmt2ANLUxUecmNjf7Py9s7N1UEJ1cxy',
   'AaYBdEnjqYoay3ozs2tY1tm9kFXxjUrAZeNZngnqwmV1',
   // 'HfEJXp9YNdrrPuqAoaeKzL3pyCvsdnVQxHXpf7KVTsjY', //? Solpunks
@@ -8,4 +8,5 @@ export const HIDDEN_POOLS = [
 
 export const CUSTOM_POOLS_NAMES = {
   HfEJXp9YNdrrPuqAoaeKzL3pyCvsdnVQxHXpf7KVTsjY: 'solpunks',
+  '3da5KmaRp9gHx2wzv8PdZHpxXWvbHMRUxCK9FCLvHGZr': 'thugbirdz',
 };
