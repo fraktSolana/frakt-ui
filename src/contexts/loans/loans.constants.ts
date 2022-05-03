083289282ed9dcdb3df7dbfb060dfe41d22d177b
sablevsky <vladislav1145@gmail.com>
2022-05-03 13:11:26 +0400

    Fix discount collections

--- src/contexts/loans/loans.constants.ts
+++ src/contexts/loans/loans.constants.ts
@@ -3,6 +3,6 @@ export const MOCK_VALUATION = (MOCK_LOAN_TO_VALUE / 30) * 100;
 
 export const DISCOUNT_NFT_CREATORS = [
   // '6wPYbuGRXZjVw2tCeTxwRiQU7AzFDTeFEKuUFpJZpcix',
-  '5WumDfbLVQSjJ63P2Dqxsw4v7w3LwNTDLsCsCF9FvFh5',
   '2vQfvY7inzfgVjzzxoNGEc7ZYmChqAWZJAtVUQXtgotx',
+  // '2vQfvY7inzfgVjzzxoNGEc7ZYmChqAWZJAtVUQXtgotx',
 ];
