443bfbf9d34bf6036149acfe1931fde4072c57ac
sablevsky <vladislav1145@gmail.com>
2022-05-31 14:57:23 +0400

    Fix solana timestamp hook

--- src/hooks/useSolanaTimestamp.ts
+++ src/hooks/useSolanaTimestamp.ts
@@ -1,25 +1,18 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { commonActions } from './../state/common/actions';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 import { selectSolanaTimestamp } from '../state/common/selectors';
 
 type UseSolanaTimestamp = () => number | null;
 
 export const useSolanaTimestamp: UseSolanaTimestamp = () => {
-  const [solanaTimestamp, setSolanaTimestamp] = useState<number | null>(null);
   const dispatch = useDispatch();
-  const solanaTimestampData = useSelector(selectSolanaTimestamp);
+  const solanaTimestamp = useSelector(selectSolanaTimestamp);
 
   useEffect(() => {
     dispatch(commonActions.fetchSolanaTimestamp());
   }, [dispatch]);
-
-  useEffect(() => {
-    if (solanaTimestampData) {
-      setSolanaTimestamp(solanaTimestampData);
-    }
-  }, [solanaTimestampData]);
 
   return solanaTimestamp;
 };
