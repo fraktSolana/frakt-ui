aa0b67069907e40d017c8d06c21ac22b606c5d86
sablevsky <vladislav1145@gmail.com>
2022-06-15 14:49:50 +0400

    Add typings

--- src/state/loans/sagas.ts
+++ src/state/loans/sagas.ts
@@ -1,7 +1,9 @@
 import { takeLatest, all, put, call } from 'redux-saga/effects';
 import { eventChannel } from 'redux-saga';
 import { Socket } from 'socket.io-client';
+
 import { loansActions } from './actions';
+import { Lending, LoanView } from './types';
 
 const loanChannel = (socket: Socket) =>
   eventChannel((emit) => {
@@ -15,11 +17,11 @@ const lendingChannel = (socket: Socket) =>
     return () => socket.off('lending');
   });
 
-const loanSaga = function* (loans) {
+const loanSaga = function* (loans: LoanView[]) {
   yield put(loansActions.setLoans(loans));
 };
 
-const lendingSaga = function* (lending) {
+const lendingSaga = function* (lending: Lending) {
   yield put(loansActions.setLending(lending));
 };
 
