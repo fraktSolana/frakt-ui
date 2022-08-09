e928bd267bcda5679ff3ee1a7d4d79e9b95ab3c1
MaxK <max.kachanov@gmail.com>
2022-08-09 15:02:59 +0300

    fix global styles

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,7 +1,6 @@
 import RcTooltip from 'rc-tooltip';
 import { TooltipProps as RcTooltipProps } from 'rc-tooltip/lib/Tooltip';
 import 'rc-tooltip/assets/bootstrap_white.css';
-import './styles.scss';
 
 export const Tooltip = ({
   children,
