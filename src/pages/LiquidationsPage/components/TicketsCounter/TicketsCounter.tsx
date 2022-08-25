c5fceeee2d6e3323c39b86c36c9fe8050ca48cbf
MaxK <max.kachanov@gmail.com>
2022-08-25 13:23:41 +0300

    fixed ticket counter

--- src/pages/LiquidationsPage/components/TicketsCounter/TicketsCounter.tsx
+++ src/pages/LiquidationsPage/components/TicketsCounter/TicketsCounter.tsx
@@ -10,10 +10,8 @@ interface TicketsCounterProps {
 export const TicketsCounter: FC<TicketsCounterProps> = ({ tickets }) => {
   return (
     <div className={styles.ticket}>
-      <Ticket className={styles.ticketIcon} />
-      <h2 className={styles.value}>
-        Your tickets: <span>{tickets}</span>
-      </h2>
+      <div className={styles.title}>Your tickets:</div>
+      <div className={styles.value}>{tickets}</div>
     </div>
   );
 };
