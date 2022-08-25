8059654229d4e11add8144b56e1bfb44cf3b70a5
MaxK <max.kachanov@gmail.com>
2022-08-25 13:56:44 +0300

    custom theme switch

--- src/componentsNew/Header/Header.tsx
+++ src/componentsNew/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import { selectWalletModalVisible } from '../../state/common/selectors';
 import WalletContent from '../../components/WalletContent';
+import { Switch } from 'antd';
 import { Logo } from '../../iconsNew/Logo';
 import { Frakt } from '../../iconsNew/Frakt';
 import ConnectWallet from '../ConnectWallet';
@@ -8,6 +9,7 @@ import styles from './styles.module.scss';
 
 export const Header = () => {
   const visible = useSelector(selectWalletModalVisible);
+  const handleThemeChange = (evt) => {};
 
   return (
     <div className={styles.container}>
@@ -16,6 +18,7 @@ export const Header = () => {
       <Frakt />
       <div className={styles.widgetContainer}>
         <ConnectWallet />
+        <Switch className={styles.toggle} onChange={handleThemeChange} />
       </div>
     </div>
   );
