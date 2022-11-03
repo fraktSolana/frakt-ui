7dd7a898da1cfce3f194c0fb9a312c8a74decaaf
Vladislav Plaksa <ezekiel.9218@gmail.com>
2022-11-03 13:03:19 +0300

    fix: remove my profile page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,5 @@
 export { default as Page404 } from './Page404';
 export { default as HomePage } from './DashboardPage';
-export { default as WalletPage } from './WalletPage';
 export { default as LendPage } from './LendPage';
 export { default as LiquidationsPage } from './LiquidationsPage';
 export { default as LoansPage } from './LoansPage';
