fb98e996f7cb835f012d0bfd242de8ccaae7304f
sablevsky <vladislav1145@gmail.com>
2022-11-18 16:59:53 +0100

    Remove legacy tokenList logic

--- src/state/rootReducer.ts
+++ src/state/rootReducer.ts
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 
 import commonReducers from './common/reducers';
-import tokenListReducers from './tokenList/reducers';
 import loansReducers from './loans/reducers';
 import liquidationsReducer from './liquidations/reducers';
 import fetchStatsReducer from './stats/reducers';
@@ -9,7 +8,6 @@ import themeReducer from './theme/reducers';
 
 const rootReducers = combineReducers({
   common: commonReducers,
-  tokenList: tokenListReducers,
   loans: loansReducers,
   liquidations: liquidationsReducer,
   stats: fetchStatsReducer,
