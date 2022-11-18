39639621630d6e2818775a62bed55a98ff807478
sablevsky <vladislav1145@gmail.com>
2022-11-18 17:03:29 +0100

    Remove unused commented code

--- src/App.tsx
+++ src/App.tsx
@@ -20,20 +20,15 @@ import {
 import { FC } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { IntercomProvider } from 'react-use-intercom';
 
 import { Router } from './router';
 import store from './state/store';
 import { ENDPOINT } from './config';
-// import { IntercomService, INTERCOM_APP_ID } from './utils/intercom';
-// import { createBrowserHistory } from 'history';
 import { initSentry } from './utils/sentry';
 import { initAmplitude } from './utils/amplitude';
 import Confetti from './components/Confetti';
 
-// const history = createBrowserHistory();
-
-initSentry(/*history*/);
+initSentry();
 initAmplitude();
 
 const wallets = [
@@ -58,11 +53,8 @@ const App: FC = () => {
       <ConnectionProvider endpoint={ENDPOINT}>
         <WalletProvider wallets={wallets} autoConnect>
           <QueryClientProvider client={queryClient}>
-            {/* <IntercomProvider appId={INTERCOM_APP_ID}> */}
             <Router />
             <Confetti />
-            {/* <IntercomService /> */}
-            {/* </IntercomProvider> */}
           </QueryClientProvider>
         </WalletProvider>
       </ConnectionProvider>
