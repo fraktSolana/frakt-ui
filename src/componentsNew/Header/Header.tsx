16b573c85bc0105f3c0353ac7362a411f7552055
sablevsky <vladislav1145@gmail.com>
2022-11-22 18:05:02 +0100

    CSS responsive changes

--- src/componentsNew/Header/Header.tsx
+++ src/componentsNew/Header/Header.tsx
@@ -22,6 +22,7 @@ export const Header: FC = () => {
         href="https://frakt-landing.vercel.app/"
         target="_blank"
         rel="noopener noreferrer"
+        className={styles.logoWrapper}
       >
         <Logo className={styles.logo} />
       </a>
