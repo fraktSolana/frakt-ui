b7effa465a720578b7a9799750af29b6a7776d3e
sablevsky <vladislav1145@gmail.com>
2022-11-22 15:41:28 +0100

    Add new api requests for notifications

--- src/api/user/requests.ts
+++ src/api/user/requests.ts
@@ -1,12 +1,11 @@
 import { web3 } from '@frakt-protocol/frakt-sdk';
 import { getDiscordAvatarUrl } from '@frakt/utils';
 import axios from 'axios';
-import { UserInfo, UserInfoRaw } from './types';
+import { UserInfo, UserInfoRaw, Notification } from './types';
 
 type FetchUser = (props: {
   publicKey: web3.PublicKey;
 }) => Promise<UserInfo | null>;
-
 export const fetchUser: FetchUser = async ({ publicKey }) => {
   try {
     const { data } = await axios.get<UserInfoRaw>(
@@ -25,7 +24,6 @@ export const fetchUser: FetchUser = async ({ publicKey }) => {
 };
 
 type RemoveUser = (props: { publicKey: web3.PublicKey }) => Promise<void>;
-
 export const removeUser: RemoveUser = async ({ publicKey }) => {
   try {
     await axios.get(
@@ -36,4 +34,55 @@ export const removeUser: RemoveUser = async ({ publicKey }) => {
   } catch (error) {
     return null;
   }
+};
+
+type GetUserNotifications = (props: {
+  publicKey: web3.PublicKey;
+}) => Promise<ReadonlyArray<Notification>>;
+export const getUserNotifications: GetUserNotifications = async ({
+  publicKey,
+}) => {
+  try {
+    const { data } = await axios.get<ReadonlyArray<Notification>>(
+      `https://${process.env.BACKEND_DOMAIN}/history/${publicKey.toBase58()}`,
+    );
+
+    return data ?? [];
+  } catch (error) {
+    return [];
+  }
+};
+
+type MarkNotificationsAsRead = (props: {
+  publicKey: web3.PublicKey;
+  notificationIds: string[];
+}) => Promise<void>;
+export const markNotificationsAsRead: MarkNotificationsAsRead = async ({
+  publicKey,
+  notificationIds,
+}) => {
+  await axios.post(
+    `https://${process.env.BACKEND_DOMAIN}/history/${publicKey.toBase58()}`,
+    {
+      ids: notificationIds,
+    },
+  );
+};
+
+type DeleteNotifications = (props: {
+  publicKey: web3.PublicKey;
+  notificationIds: string[];
+}) => Promise<void>;
+export const deleteNotifications: DeleteNotifications = async ({
+  publicKey,
+  notificationIds,
+}) => {
+  await axios.post(
+    `https://${
+      process.env.BACKEND_DOMAIN
+    }/history/${publicKey.toBase58()}/delete`,
+    {
+      ids: notificationIds,
+    },
+  );
 };
--- src/api/user/types.ts
+++ src/api/user/types.ts
@@ -8,3 +8,23 @@ export interface UserInfo {
   avatarUrl: string;
   isOnServer: boolean;
 }
+
+export enum NotificationType {
+  LOAN = 'loan',
+  DEPOSIT = 'deposit',
+  LOT_TICKET = 'lotTicket',
+  GRACE = 'grace',
+}
+
+export interface Notification {
+  id: string;
+  type: NotificationType;
+  user: string;
+  message: {
+    title: string;
+    body: string;
+  };
+  image?: string;
+  isRead: boolean;
+  date: number;
+}
