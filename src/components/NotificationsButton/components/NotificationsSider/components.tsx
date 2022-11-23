6e861fd98d77692535d8d675e10a7e624dbec51f
sablevsky <vladislav1145@gmail.com>
2022-11-23 13:23:48 +0100

    Implement Notifications settings in sider

--- src/components/NotificationsButton/components/NotificationsSider/components.tsx
+++ src/components/NotificationsButton/components/NotificationsSider/components.tsx
@@ -2,6 +2,7 @@ import Button from '@frakt/components/Button';
 import { GoBackButton } from '@frakt/components/GoBackButton';
 import { Loader } from '@frakt/components/Loader';
 import { TextInput } from '@frakt/components/TextInput';
+import Toggle from '@frakt/components/Toggle';
 import { UserAvatar } from '@frakt/components/UserAvatar';
 import { useUserInfo, useUserNotifications } from '@frakt/hooks';
 import { Alert } from '@frakt/iconsNew/Alert';
@@ -12,7 +13,7 @@ import { shortenAddress } from '@frakt/utils/solanaUtils';
 import { useWallet } from '@solana/wallet-adapter-react';
 import classNames from 'classnames';
 import { FC, useState } from 'react';
-import { ContentType } from './constants';
+import { ContentType, NotificationsSettingsNames } from './constants';
 
 import { useNotificationsSider } from './hooks';
 import styles from './NotificationsSider.module.scss';
@@ -49,6 +50,107 @@ export const Header: FC = () => {
           Clear all
         </button>
       )}
+    </div>
+  );
+};
+
+export const NoNotificationsContent: FC = () => {
+  return (
+    <div className={classNames(styles.content, styles.noNotifications)}>
+      <BellSlash />
+      <p className={styles.noNotificationsText}>
+        You have no new notifications
+      </p>
+    </div>
+  );
+};
+
+export const LoadingContent: FC = () => {
+  return (
+    <div className={classNames(styles.content, styles.contentCentered)}>
+      <Loader size="large" />
+    </div>
+  );
+};
+
+export const SettingsContent: FC = () => {
+  const { publicKey } = useWallet();
+
+  const { settings, changeSettings } = useUserNotifications();
+
+  const { data, isDiscordConnected, removeUserInfo } = useUserInfo();
+
+  const linkButtonHanlder = async () => {
+    if (isDiscordConnected) {
+      await removeUserInfo();
+      return;
+    }
+
+    window.location.href = getDiscordUri(publicKey);
+  };
+
+  return (
+    <div className={classNames(styles.content)}>
+      <h2 className={styles.contentTitle}>Channels</h2>
+
+      <p className={styles.discordSettingsLabel}>Discord</p>
+      <div className={styles.discordSettings}>
+        <UserAvatar
+          className={styles.discordSettingsAvatar}
+          imageUrl={data?.avatarUrl}
+        />
+        <div className={styles.discordSettingsUserInfo}>
+          <p className={styles.discordSettingsUserName}>
+            {data ? shortenAddress(publicKey.toBase58()) : 'Username'}
+          </p>
+          <Button
+            className={styles.discordSettingsBtn}
+            onClick={linkButtonHanlder}
+          >
+            {isDiscordConnected ? 'Unlink' : 'Link'}
+          </Button>
+        </div>
+      </div>
+      <div className={styles.discordAlert}>
+        <Alert />
+        <p>
+          Please note that you should be a member of our{' '}
+          <a
+            href={process.env.FRAKT_DISCORD_SERVER}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.discordLink}
+          >
+            server
+          </a>{' '}
+          to receive notifications
+        </p>
+      </div>
+
+      <h2
+        className={classNames(
+          styles.contentTitle,
+          styles.contentTitleOffsetTop,
+        )}
+      >
+        Events
+      </h2>
+      <div className={styles.eventsList}>
+        {Object.entries(settings).map(([label, value]) => (
+          <div key={label} className={styles.eventsListRow}>
+            <p>
+              {NotificationsSettingsNames[label] ||
+                NotificationsSettingsNames.DEFAULT}
+            </p>
+            <Toggle
+              defaultChecked={value}
+              onChange={(value) =>
+                changeSettings({ ...settings, [label]: value })
+              }
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
@@ -106,80 +208,6 @@ export const SetUpContent: FC = () => {
       <Button type="secondary" onClick={onSubmit}>
         Save
       </Button>
-    </div>
-  );
-};
-
-export const NoNotificationsContent: FC = () => {
-  return (
-    <div className={classNames(styles.content, styles.noNotifications)}>
-      <BellSlash />
-      <p className={styles.noNotificationsText}>
-        You have no new notifications
-      </p>
-    </div>
-  );
-};
-
-export const LoadingContent: FC = () => {
-  return (
-    <div className={classNames(styles.content, styles.contentCentered)}>
-      <Loader size="large" />
-    </div>
-  );
-};
-
-export const SettingsContent: FC = () => {
-  const { publicKey } = useWallet();
-
-  const { data, isDiscordConnected, removeUserInfo } = useUserInfo();
-
-  const linkButtonHanlder = async () => {
-    if (isDiscordConnected) {
-      await removeUserInfo();
-      return;
-    }
-
-    window.location.href = getDiscordUri(publicKey);
-  };
-
-  return (
-    <div className={classNames(styles.content)}>
-      <h2 className={styles.contentTitle}>Channels</h2>
-
-      <p className={styles.discordSettingsLabel}>Discord</p>
-      <div className={styles.discordSettings}>
-        <UserAvatar
-          className={styles.discordSettingsAvatar}
-          imageUrl={data?.avatarUrl}
-        />
-        <div className={styles.discordSettingsUserInfo}>
-          <p className={styles.discordSettingsUserName}>
-            {data ? shortenAddress(publicKey.toBase58()) : 'Username'}
-          </p>
-          <Button
-            className={styles.discordSettingsBtn}
-            onClick={linkButtonHanlder}
-          >
-            {isDiscordConnected ? 'Unlink' : 'Link'}
-          </Button>
-        </div>
-      </div>
-      <div className={styles.discordAlert}>
-        <Alert />
-        <p>
-          Please note that you should be a member of our{' '}
-          <a
-            href={process.env.FRAKT_DISCORD_SERVER}
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.discordLink}
-          >
-            server
-          </a>{' '}
-          to receive notifications
-        </p>
-      </div>
     </div>
   );
 };
--- src/components/NotificationsButton/components/NotificationsSider/constants.ts
+++ src/components/NotificationsButton/components/NotificationsSider/constants.ts
@@ -1,3 +1,5 @@
+import { NotificationEvents } from '@frakt/api/user';
+
 export enum ContentType {
   LOADING = 'loading',
   SIGN_MESSAGE = 'sign message',
@@ -5,3 +7,12 @@ export enum ContentType {
   SETTINGS = 'settings',
   NOTIFICATIONS = 'notifications',
 }
+
+export const NotificationsSettingsNames = {
+  [NotificationEvents.LOANS]: 'Loans',
+  [NotificationEvents.DEPOSITS]: 'Deposits',
+  [NotificationEvents.LOT_TICKETS]: 'Lottery tickets',
+  [NotificationEvents.GRACES]: 'Grace period',
+  [NotificationEvents.LIQUIDATIONS_IN_24H]: 'Liquidations in 24h',
+  DEFAULT: 'Unknown value',
+};
