652da539baa24caf89b106c4af6f814e7c9a7a5d
sablevsky <vladislav1145@gmail.com>
2022-12-21 20:32:33 +0100

    Fix env.local and constants

--- src/utils/bonds/constants.ts
+++ src/utils/bonds/constants.ts
@@ -7,5 +7,5 @@ export const BONDS_PROGRAM_PUBKEY = new web3.PublicKey(
   process.env.BONDS_DEVNET_PROGRAM_PUBKEY,
 ); //TODO: Replace to mainnet
 export const BONDS_VALIDATION_PROGRAM_PUBKEY = new web3.PublicKey(
-  process.env.BONDS_VALIDATION_DEVNET,
+  process.env.BONDS_VALIDATION_DEVNTET_PROGRAM_PUBKEY,
 ); //TODO: Replace to mainnet
