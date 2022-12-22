2e2793d7b7f0f409a9dde65659c7073e073030d5
sablevsky <vladislav1145@gmail.com>
2022-12-22 16:59:24 +0100

    Use useMarket on MarketPage

--- src/pages/MarketPage/MarketPage.tsx
+++ src/pages/MarketPage/MarketPage.tsx
@@ -1,6 +1,6 @@
 import { AppLayout } from '@frakt/components/Layout/AppLayout';
 import { FC, useRef, useState } from 'react';
-import mockImg from './mockImg.jpg';
+import { web3 } from 'fbonds-core';
 
 import styles from './MarketPage.module.scss';
 import Button from '@frakt/components/Button';
@@ -17,8 +17,11 @@ import { SearchInput } from '@frakt/components/SearchInput';
 import Bond from './components/Bond/Bond';
 import OrderBook from './components/OrderBook/OrderBook';
 import { Arrow } from '@frakt/iconsNew/Arrow';
-import { useHistory, useParams } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
 import { useOnClickOutside } from '@frakt/hooks';
+import { useMarket } from './hooks';
+import { PATHS } from '@frakt/constants';
+import { Loader } from '@frakt/components/Loader';
 
 export enum InputControlsNames {
   SHOW_STAKED = 'showStaked',
@@ -34,7 +37,6 @@ const collectionsMock = [
 const MarketPage: FC = () => {
   const { marketPubkey } = useParams<{ marketPubkey: string }>();
 
-  const history = useHistory();
   const [selectedCollections, setSelectedCollections] = useState<string[]>([]);
   const {
     control,
@@ -48,108 +50,111 @@ const MarketPage: FC = () => {
     toggle: toggleFiltersModal,
   } = useFiltersModal();
 
-  const goBack = () => {
-    history.goBack();
-  };
-
   const ref = useRef();
   useOnClickOutside(ref, closeFiltersModal);
 
+  const { market, isLoading } = useMarket({
+    marketPubkey: new web3.PublicKey(marketPubkey),
+  });
+
   return (
     <AppLayout>
-      <div className={styles.bondPage}>
-        <>
-          <div className={styles.wrapper}>
-            <div>
-              <div onClick={goBack} className={styles.btnBack}>
-                <Arrow />
+      {isLoading && <Loader size="large" />}
+      {!!market && (
+        <div className={styles.bondPage}>
+          <>
+            <div className={styles.wrapper}>
+              <div>
+                <NavLink to={PATHS.BONDS} className={styles.btnBack}>
+                  <Arrow />
+                </NavLink>
               </div>
-            </div>
 
-            <div className={styles.bondMarket}>
-              <div className={styles.bondMarketInfo}>
-                <img src={mockImg} className={styles.image} />
-                <div className={styles.title}>Solana Monkey Business</div>
-              </div>
-              <div className={styles.info}>
-                <div className={styles.infoItem}>
-                  <div className={styles.infoItemBalance}>
-                    <div className={styles.infoName}>BALANCE</div>
-                    <div className={styles.infoValue}>4,356,456.7 fndSMB</div>
-                  </div>
+              <div className={styles.bondMarket}>
+                <div className={styles.bondMarketInfo}>
+                  <img src={market.image} className={styles.image} />
+                  <div className={styles.title}>{market.name}</div>
                 </div>
-                <div className={styles.infoItem}>
-                  <div>
-                    <div className={styles.infoName}>REWARDS</div>
-                    <div className={styles.infoValue}>345 SOL</div>
-                  </div>
-                  <Button className={styles.btn} type="primary">
-                    Redeem
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={styles.sortWrapper}>
-            <SearchInput
-              className={styles.searchInput}
-              placeholder="Search by name"
-            />
-            <div ref={ref}>
-              <div className={styles.filtersWrapper}>
-                <Button type="tertiary" onClick={toggleFiltersModal}>
-                  Filters
-                </Button>
-
-                {filtersModalVisible && (
-                  <FiltersDropdown
-                    onCancel={closeFiltersModal}
-                    className={styles.filtersDropdown}
-                  >
-                    <div>
-                      {showStakedOnlyToggle && (
-                        <Controller
-                          control={control}
-                          name={InputControlsNames.SHOW_STAKED}
-                          render={({ field: { ref, ...field } }) => (
-                            <Toggle
-                              label="My bag only"
-                              className={styles.toggle}
-                              name={InputControlsNames.SHOW_STAKED}
-                              {...field}
-                            />
-                          )}
-                        />
-                      )}
-                      <FilterCollections
-                        options={collectionsMock}
-                        selectedCollections={selectedCollections}
-                        setSelectedCollections={setSelectedCollections}
-                      />
-                      <SortControl
-                        control={control}
-                        name={InputControlsNames.SORT}
-                        options={SORT_VALUES}
-                        sort={sort}
-                        setValue={setValue}
-                      />
+                <div className={styles.info}>
+                  <div className={styles.infoItem}>
+                    <div className={styles.infoItemBalance}>
+                      <div className={styles.infoName}>BALANCE</div>
+                      <div className={styles.infoValue}>4,356,456.7 fndSMB</div>
                     </div>
-                  </FiltersDropdown>
-                )}
+                  </div>
+                  <div className={styles.infoItem}>
+                    <div>
+                      <div className={styles.infoName}>REWARDS</div>
+                      <div className={styles.infoValue}>345 SOL</div>
+                    </div>
+                    <Button className={styles.btn} type="primary">
+                      Redeem
+                    </Button>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
+            <div className={styles.sortWrapper}>
+              <SearchInput
+                className={styles.searchInput}
+                placeholder="Search by name"
+              />
+              <div ref={ref}>
+                <div className={styles.filtersWrapper}>
+                  <Button type="tertiary" onClick={toggleFiltersModal}>
+                    Filters
+                  </Button>
 
-          <div className={styles.bondList}>
-            <Bond />
-            <Bond />
-            <Bond />
-            <Bond />
-            <Bond />
-          </div>
-        </>
-        <OrderBook marketPubkey={marketPubkey} />
-      </div>
+                  {filtersModalVisible && (
+                    <FiltersDropdown
+                      onCancel={closeFiltersModal}
+                      className={styles.filtersDropdown}
+                    >
+                      <div>
+                        {showStakedOnlyToggle && (
+                          <Controller
+                            control={control}
+                            name={InputControlsNames.SHOW_STAKED}
+                            render={({ field: { ref, ...field } }) => (
+                              <Toggle
+                                label="My bag only"
+                                className={styles.toggle}
+                                name={InputControlsNames.SHOW_STAKED}
+                                {...field}
+                              />
+                            )}
+                          />
+                        )}
+                        <FilterCollections
+                          options={collectionsMock}
+                          selectedCollections={selectedCollections}
+                          setSelectedCollections={setSelectedCollections}
+                        />
+                        <SortControl
+                          control={control}
+                          name={InputControlsNames.SORT}
+                          options={SORT_VALUES}
+                          sort={sort}
+                          setValue={setValue}
+                        />
+                      </div>
+                    </FiltersDropdown>
+                  )}
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.bondList}>
+              <Bond />
+              <Bond />
+              <Bond />
+              <Bond />
+              <Bond />
+            </div>
+          </>
+          <OrderBook marketPubkey={marketPubkey} />
+        </div>
+      )}
     </AppLayout>
   );
 };
