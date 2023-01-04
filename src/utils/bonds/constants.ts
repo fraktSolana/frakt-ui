e2137b8cd17881bda5be9a19311805f4421f5e98
sablevsky <vladislav1145@gmail.com>
2023-01-04 12:57:47 +0100

    Add constants for bonds from env files

--- src/utils/bonds/constants.ts
+++ src/utils/bonds/constants.ts
@@ -1,11 +1,14 @@
 import { web3 } from 'fbonds-core';
 
+export const FRAKT_MARKET_PROGRAM_PUBKEY = new web3.PublicKey(
+  process.env.FRAKT_MARKET_PROGRAM_PUBKEY,
+);
 export const CROSS_MINT_AMM_PROGRAM_PUBKEY = new web3.PublicKey(
-  process.env.CROSS_MINT_AMM_DEVNET_PROGRAM_PUBKEY,
-); //TODO: Replace to mainnet
+  process.env.CROSS_MINT_AMM_PROGRAM_PUBKEY,
+);
 export const BONDS_PROGRAM_PUBKEY = new web3.PublicKey(
-  process.env.BONDS_DEVNET_PROGRAM_PUBKEY,
-); //TODO: Replace to mainnet
+  process.env.BONDS_PROGRAM_PUBKEY,
+);
 export const BONDS_VALIDATION_PROGRAM_PUBKEY = new web3.PublicKey(
-  process.env.BONDS_VALIDATION_DEVNTET_PROGRAM_PUBKEY,
-); //TODO: Replace to mainnet
+  process.env.BONDS_VALIDATION_PROGRAM_PUBKEY,
+);
