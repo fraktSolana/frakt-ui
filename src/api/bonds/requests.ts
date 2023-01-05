8bb4b361cfa91cdbacdcefbcbbfb65c8c3c156a9
sablevsky <vladislav1145@gmail.com>
2023-01-05 16:53:08 +0100

    Add backend test domain

--- src/api/bonds/requests.ts
+++ src/api/bonds/requests.ts
@@ -1,10 +1,10 @@
 import { web3 } from '@frakt-protocol/frakt-sdk';
 import axios from 'axios';
-import { BorrowNft } from '../nft';
 
 import { MarketPreview, Market, Pair } from './types';
 
-const BACKEND_DOMAIN = process.env.BACKEND_DOMAIN;
+//TODO: Change to main backend on release
+const BACKEND_DOMAIN = process.env.BACKEND_TEST_DOMAIN;
 
 type FetchAllMarkets = () => Promise<Market[]>;
 export const fetchAllMarkets: FetchAllMarkets = async () => {
@@ -36,25 +36,6 @@ export const fetchMarketsPreview: FetchMarketsPreview = async ({
 }) => {
   const { data } = await axios.get<MarketPreview[]>(
     `https://${BACKEND_DOMAIN}/bonds/preview/${walletPubkey?.toBase58() ?? ''}`,
-  );
-
-  return data;
-};
-
-type FetchWalletBorrowNfts = (props: {
-  walletPubkey: web3.PublicKey;
-  limit?: number;
-  offset?: number;
-}) => Promise<BorrowNft[]>;
-export const fetchWalletBorrowNfts: FetchWalletBorrowNfts = async ({
-  walletPubkey,
-  limit = 1000,
-  offset = 0,
-}) => {
-  const { data } = await axios.get<BorrowNft[]>(
-    `https://${
-      process.env.BACKEND_DOMAIN
-    }/bonds/nft/meta/${walletPubkey?.toBase58()}?limit=${limit}&offset=${offset}`,
   );
 
   return data;
