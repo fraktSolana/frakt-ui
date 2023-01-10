f325f39fbc85520ee72d3cf86ae5fefaf8c16bd6
sablevsky <vladislav1145@gmail.com>
2023-01-10 11:25:54 +0100

    Implement useMarketPairs hook

--- src/utils/bonds/hooks.ts
+++ src/utils/bonds/hooks.ts
@@ -1,5 +1,6 @@
-import { fetchAllMarkets } from './../../api/bonds/requests';
+import { fetchAllMarkets, fetchMarketPairs, Pair } from '@frakt/api/bonds';
 import { useQuery } from '@tanstack/react-query';
+import { web3 } from 'fbonds-core';
 
 export const useMarkets = () => {
   const { data, isLoading } = useQuery(['markets'], () => fetchAllMarkets(), {
@@ -9,6 +10,27 @@ export const useMarkets = () => {
 
   return {
     markets: data || null,
+    isLoading,
+  };
+};
+
+type UseMarketPairs = (props: { marketPubkey: string }) => {
+  pairs: Pair[];
+  isLoading: boolean;
+};
+export const useMarketPairs: UseMarketPairs = ({ marketPubkey }) => {
+  const { data, isLoading } = useQuery(
+    ['marketPairs', marketPubkey],
+    () => fetchMarketPairs({ marketPubkey: new web3.PublicKey(marketPubkey) }),
+    {
+      enabled: !!marketPubkey,
+      staleTime: 30 * 1000, //? 30sec
+      refetchOnWindowFocus: false,
+    },
+  );
+
+  return {
+    pairs: data || [],
     isLoading,
   };
 };
