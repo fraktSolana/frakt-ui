5d2d86a2021efbbc2287fa347ac3f8e8e52f1543
sablevsky <vladislav1145@gmail.com>
2023-01-11 14:46:53 +0100

    Add getRepaidCollateral ixn into makeRepayBondTransaction

--- src/utils/bonds/transactions/makeRepayBondTransaction.tsx
+++ src/utils/bonds/transactions/makeRepayBondTransaction.tsx
@@ -8,6 +8,7 @@ type MakeRepayBondTransaction = (params: {
   bondPubkey: string;
   bondTokenMint: string;
   bondCollateralOrSolReceiver: string;
+  collateralTokenMint: string;
   connection: web3.Connection;
   wallet: WalletContextState;
 }) => Promise<{
@@ -19,26 +20,43 @@ export const makeRepayBondTransaction: MakeRepayBondTransaction = async ({
   bondPubkey,
   bondTokenMint,
   bondCollateralOrSolReceiver,
+  collateralTokenMint,
   wallet,
   connection,
 }) => {
-  const { instructions, signers } = await management.repayFBond({
-    accounts: {
-      adminPubkey: BONDS_ADMIN_PUBKEY,
-      fbond: new web3.PublicKey(bondPubkey),
-      fbondsTokenMint: new web3.PublicKey(bondTokenMint),
-      bondCollateralOrSolReceiver: new web3.PublicKey(
-        bondCollateralOrSolReceiver,
-      ),
-      userPubkey: wallet.publicKey,
-    },
-    connection,
-    programId: BONDS_PROGRAM_PUBKEY,
-    sendTxn: sendTxnPlaceHolder,
-  });
+  const { instructions: repayIxs, signers: repaySigners } =
+    await management.repayFBond({
+      accounts: {
+        adminPubkey: BONDS_ADMIN_PUBKEY,
+        fbond: new web3.PublicKey(bondPubkey),
+        fbondsTokenMint: new web3.PublicKey(bondTokenMint),
+        bondCollateralOrSolReceiver: new web3.PublicKey(
+          bondCollateralOrSolReceiver,
+        ),
+        userPubkey: wallet.publicKey,
+      },
+      connection,
+      programId: BONDS_PROGRAM_PUBKEY,
+      sendTxn: sendTxnPlaceHolder,
+    });
+
+  const { instructions: getCollateralIxs, signers: getCollateralSigners } =
+    await management.getRepaidCollateral({
+      accounts: {
+        collateralTokenMint: new web3.PublicKey(collateralTokenMint),
+        fbond: new web3.PublicKey(bondPubkey),
+        userPubkey: wallet.publicKey,
+      },
+      args: {
+        nextBoxIndex: '0',
+      },
+      connection,
+      programId: BONDS_PROGRAM_PUBKEY,
+      sendTxn: sendTxnPlaceHolder,
+    });
 
   return {
-    transaction: new web3.Transaction().add(...instructions),
-    signers,
+    transaction: new web3.Transaction().add(...repayIxs, ...getCollateralIxs),
+    signers: [repaySigners, getCollateralSigners].flat(),
   };
 };
