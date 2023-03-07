efcd1e28e3551f65af86bf3b5b1dd035714c9c4d
Vladislav Plaksa <ezekiel.9218@gmail.com>
2023-03-07 14:37:39 +0300

    remove signAndSendTransactionsInSeries

--- src/utils/transactions/helpers/index.ts
+++ src/utils/transactions/helpers/index.ts
@@ -2,4 +2,3 @@ export * from './signAndConfirmTransaction';
 export * from './showSolscanLinkNotification';
 export * from './mergeIxsIntoTxn';
 export * from './signAndSendAllTransactions';
-export * from './signAndSendTransactionsInSeries';
