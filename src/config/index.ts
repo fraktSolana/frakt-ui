8e01ac421bb065e6b8a18cc366a388b150d43b03
Vedamir Efanov <vedamire@protonmail.com>
2023-03-12 11:38:09 +0400

    fixed  IS_PRIVATE_MARKETS env bug

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,4 +7,7 @@ export const ENDPOINT = IS_DEVELOPMENT
 export const FRKT_TOKEN_MINT_PUBLIC_KEY =
   'ErGB9xa24Szxbk1M28u2Tx8rKPqzL6BroNkkzk5rG4zj';
 
-export const IS_PRIVATE_MARKETS = !!process.env.IS_PRIVATE_MARKETS;
+export const IS_PRIVATE_MARKETS =
+  process.env.IS_PRIVATE_MARKETS !== undefined
+    ? process.env.IS_PRIVATE_MARKETS
+    : false;
