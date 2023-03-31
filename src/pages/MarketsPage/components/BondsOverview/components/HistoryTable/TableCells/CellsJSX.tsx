9dc4d141e775e165be1abb95dc1c2e7da77141a5
Vladislav Plaksa <ezekiel.9218@gmail.com>
2023-03-31 14:35:23 +0300

    Fix capitalize text on cells

--- src/pages/MarketsPage/components/BondsOverview/components/HistoryTable/TableCells/CellsJSX.tsx
+++ src/pages/MarketsPage/components/BondsOverview/components/HistoryTable/TableCells/CellsJSX.tsx
@@ -2,28 +2,15 @@ import moment from 'moment';
 import classNames from 'classnames';
 
 import { Solana } from '@frakt/icons';
-import { Bond, BondStats } from '@frakt/api/bonds';
+import { BondStats } from '@frakt/api/bonds';
 
 import styles from './TableCells.module.scss';
 
 export const createAutocompoundJSX = (autocompound: string) => (
-  <span className={styles.value}>{autocompound || '--'}</span>
+  <span className={classNames(styles.value, styles.capitalizeValue)}>
+    {autocompound || '--'}
+  </span>
 );
-
-export const createReceiveJSX = (value = 0) => {
-  if (typeof value === 'string')
-    return <span className={styles.value}>{value}</span>;
-
-  return (
-    <span
-      className={classNames(styles.value, {
-        [styles.negative]: value < 0,
-      })}
-    >
-      {value?.toFixed(2) || '--'} <Solana />
-    </span>
-  );
-};
 
 export const createliquidatingAtJSX = (value = 0) => (
   <span className={styles.value}>
@@ -35,7 +22,11 @@ export const ReceiveCell = ({ stats }: { stats: BondStats }) => {
   const { received, state } = stats;
 
   if (typeof received === 'string')
-    return <span className={styles.value}>{received}</span>;
+    return (
+      <span className={classNames(styles.value, styles.capitalizeValue)}>
+        {received}
+      </span>
+    );
 
   return (
     <span
