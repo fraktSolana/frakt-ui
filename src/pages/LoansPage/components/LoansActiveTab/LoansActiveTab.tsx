f624d39bb835d92a2b3e25a42283819ef68fc87b
Vladislav Plaksa <ezekiel.9218@gmail.com>
2023-04-04 17:44:39 +0300

    Implement bulk repay on loans table

--- src/pages/LoansPage/components/LoansActiveTab/LoansActiveTab.tsx
+++ src/pages/LoansPage/components/LoansActiveTab/LoansActiveTab.tsx
@@ -1,21 +1,45 @@
 import { FC } from 'react';
+
+import { Button } from '@frakt/components/Button';
 import { Loan } from '@frakt/api/loans';
 
 import { LoansTable } from '../LoansTable';
+import { useActiveLoans } from './hooks';
 
 import styles from './LoansActiveTab.module.scss';
+import { LoadingModal } from '@frakt/components/LoadingModal';
+
 interface LoansActiveTabProps {
   loans: Loan[];
   isLoading: boolean;
 }
 
 const LoansActiveTab: FC<LoansActiveTabProps> = ({ loans, isLoading }) => {
+  const { onBulkRepay, totalBorrowed, loadingModalVisible, closeLoadingModal } =
+    useActiveLoans();
+
   return (
-    <LoansTable
-      className={styles.loansTable}
-      data={loans}
-      loading={isLoading}
-    />
+    <>
+      <div className={styles.loanActiveTab}>
+        <Button
+          onClick={onBulkRepay}
+          className={styles.repayButton}
+          disabled={!totalBorrowed}
+        >
+          Bulk repay {totalBorrowed?.toFixed(2)} SOL
+        </Button>
+        <LoansTable
+          className={styles.loansTable}
+          data={loans}
+          loading={isLoading}
+        />
+      </div>
+      <LoadingModal
+        title="Please approve transaction"
+        visible={loadingModalVisible}
+        onCancel={closeLoadingModal}
+      />
+    </>
   );
 };
 
