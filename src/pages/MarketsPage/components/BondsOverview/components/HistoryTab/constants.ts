32d0f561dc7878ab547553ab18a2e4a3ebe27939
Vedamir Efanov <vedamire@protonmail.com>
2023-04-11 19:12:03 +0400

    done

--- src/pages/MarketsPage/components/BondsOverview/components/HistoryTab/constants.ts
+++ src/pages/MarketsPage/components/BondsOverview/components/HistoryTab/constants.ts
@@ -2,6 +2,10 @@ import { RBOption } from '@frakt/components/RadioButton';
 
 export const HISTORY_FILTER_OPTIONS: RBOption<string>[] = [
   {
+    label: 'All',
+    value: 'all',
+  },
+  {
     label: 'Issued',
     value: 'creation',
   },
