8f12478c87e7f77af6f9813382c5a7f407df71d6
Vedamir Efanov <vedamire@protonmail.com>
2023-04-28 15:28:25 +0400

    changed to new endpoints

--- src/api/bonds/requests.ts
+++ src/api/bonds/requests.ts
@@ -94,7 +94,7 @@ export const fetchAllBonds: FetchAllBonds = async ({
     : '';
 
   const { data } = await axios.get<Bond[]>(
-    `https://${BACKEND_DOMAIN}/bonds-v2?sort=${order}&skip=${skip}&limit=${limit}&sortBy=${sortBy}&${marketQuery}${walletQuery}&isPrivate=${IS_PRIVATE_MARKETS}`,
+    `https://${BACKEND_DOMAIN}/bonds?sort=${order}&skip=${skip}&limit=${limit}&sortBy=${sortBy}&${marketQuery}${walletQuery}&isPrivate=${IS_PRIVATE_MARKETS}&version=2`,
   );
 
   return data;
@@ -144,7 +144,7 @@ export const fetchBondsHistory: FetchBondsHistory = async ({
   const eventTypeQuery = eventType ? `eventType=${eventType}` : '';
 
   const { data } = await axios.get<Bond[]>(
-    `https://${BACKEND_DOMAIN}/bonds/history?sort=${order}&skip=${skip}&limit=${limit}&sortBy=${sortBy}&${marketQuery}${walletQuery}${eventTypeQuery}&isPrivate=${IS_PRIVATE_MARKETS}`,
+    `https://${BACKEND_DOMAIN}/bonds/history?sort=${order}&skip=${skip}&limit=${limit}&sortBy=${sortBy}&${marketQuery}${walletQuery}${eventTypeQuery}&isPrivate=${IS_PRIVATE_MARKETS}&version=2`,
   );
 
   return data;
