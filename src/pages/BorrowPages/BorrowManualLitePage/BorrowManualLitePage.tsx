ab8bdb4a0833c43df36eee4131db70d28ef019b8
sablevsky <vladislav1145@gmail.com>
2023-05-04 14:38:53 +0300

    Save changes

--- src/pages/BorrowPages/BorrowManualLitePage/BorrowManualLitePage.tsx
+++ src/pages/BorrowPages/BorrowManualLitePage/BorrowManualLitePage.tsx
@@ -1,21 +1,32 @@
-import { FC } from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
 import classNames from 'classnames';
 
 import { AppLayout } from '@frakt/components/Layout/AppLayout';
 import InfinityScroll from '@frakt/components/InfinityScroll';
 import { useDebounce } from '@frakt/hooks';
-import { BorrowNft } from '@frakt/api/nft';
+import {
+  BorrowNft,
+  fetchBulkSuggestion,
+  fetchMaxBorrowValue,
+} from '@frakt/api/nft';
 
-import { BorrowHeader } from '../components/BorrowHeader';
 import { Filters } from './components/Filters';
 import { useWalletNfts } from './hooks';
 import { NftCard } from './components/NftCard';
 import styles from './BorrowManualLitePage.module.scss';
 import { Sidebar } from './components/Sidebar';
-import { useBorrow } from '../cartState';
+import { CartOrder, useBorrow } from '../cartState';
 import { BulkForm } from './components/BulkForm';
+import { web3 } from 'fbonds-core';
+import { useQuery } from '@tanstack/react-query';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { LoanType } from '@frakt/api/loans';
 
 export const BorrowManualLitePage: FC = () => {
+  const wallet = useWallet();
+  const { maxBorrowValue, isLoading: maxBorrowValueLoading } =
+    useMaxBorrowValue({ walletPublicKey: wallet?.publicKey });
+
   const {
     cartOrders,
     onSelectNft,
@@ -23,7 +34,45 @@ export const BorrowManualLitePage: FC = () => {
     currentNft,
     findOrderInCart,
     onRemoveNft,
+    clearCart,
+    clearCurrentNftState,
+    setCartState,
+    setCurrentNftFromOrder,
   } = useBorrow();
+
+  const [suggestionRequested, setSuggestionRequested] =
+    useState<boolean>(false);
+
+  const [borrowValue, setBorrowValue] = useState<string>('');
+  const [borrowPercentValue, setBorrowPercentValue] = useState<number>(0);
+
+  const onBorrowPercentChange = useCallback(
+    (nextValue: number) => {
+      const depositValue =
+        ((nextValue * maxBorrowValue) / 100)?.toFixed(2) || '0';
+      setBorrowValue(depositValue);
+      setBorrowPercentValue(nextValue);
+    },
+    [maxBorrowValue],
+  );
+
+  const onBorrowValueChange = useCallback(
+    (nextValue: string) => {
+      setBorrowValue(nextValue);
+
+      const balancePercent = (parseFloat(nextValue) / maxBorrowValue) * 100;
+
+      if (balancePercent > 100) {
+        return setBorrowPercentValue(100);
+      }
+      if (balancePercent < 0) {
+        return setBorrowPercentValue(0);
+      }
+
+      return setBorrowPercentValue(balancePercent);
+    },
+    [maxBorrowValue],
+  );
 
   const {
     nfts,
@@ -49,9 +98,86 @@ export const BorrowManualLitePage: FC = () => {
     onSelectNft(nft);
   };
 
+  const { isLoading: suggestionLoading, refetch: fetchSuggestion } = useQuery(
+    ['bulkSuggestion', borrowValue],
+    () =>
+      fetchBulkSuggestion({
+        publicKey: wallet.publicKey,
+        totalValue: borrowValue,
+      }),
+    {
+      enabled: false,
+      staleTime: 5 * 60 * 1000,
+      refetchOnWindowFocus: false,
+      onSuccess: (bulkSuggestion) => {
+        const bestSuggestion = bulkSuggestion?.best || bulkSuggestion?.max;
+        if (!bestSuggestion) return;
+        clearCart();
+        clearCurrentNftState();
+        const cartOrders: CartOrder[] = bestSuggestion.orders.map((order) => ({
+          borrowNft: order.borrowNft,
+          loanType: order.loanType,
+          loanValue: order.loanValue,
+          bondOrderParams:
+            order.loanType === LoanType.BOND
+              ? {
+                  market: bestSuggestion.markets.find(
+                    ({ marketPubkey }) =>
+                      marketPubkey ===
+                      order?.borrowNft?.bondParams?.marketPubkey,
+                  ),
+                  orderParams: order.bondOrderParams,
+                }
+              : null,
+        }));
+
+        setCartState({
+          orders: cartOrders,
+          pairs: bestSuggestion?.modifiedPairs,
+        });
+      },
+    },
+  );
+
+  const debouncedRefetch = useDebounce(() => {
+    setSuggestionRequested(true);
+    fetchSuggestion();
+  }, 300);
+
+  useEffect(() => {
+    if (parseFloat(borrowValue)) {
+      debouncedRefetch();
+    }
+  }, [borrowValue, debouncedRefetch]);
+
+  useEffect(() => {
+    if (suggestionRequested && cartOrders.length && !suggestionLoading) {
+      setCurrentNftFromOrder(cartOrders[0]?.borrowNft?.mint);
+      setSuggestionRequested(false);
+    }
+  }, [
+    cartOrders,
+    suggestionRequested,
+    setCurrentNftFromOrder,
+    suggestionLoading,
+  ]);
+
+  const isNotEnoughBalanceError =
+    parseFloat(borrowValue) > parseFloat(maxBorrowValue.toFixed(2));
+
   return (
     <AppLayout>
-      <BulkForm />
+      {wallet.connected && !maxBorrowValueLoading && (
+        <BulkForm
+          borrowValue={borrowValue}
+          borrowPercentValue={borrowPercentValue}
+          onBorrowValueChange={onBorrowValueChange}
+          onBorrowPercentChange={onBorrowPercentChange}
+          maxBorrowValue={maxBorrowValue}
+          isNotEnoughBalanceError={isNotEnoughBalanceError}
+        />
+      )}
+
       {!!currentNft && <Sidebar />}
       <div
         className={classNames([
@@ -59,15 +185,6 @@ export const BorrowManualLitePage: FC = () => {
           { [styles.contentSidebarVisible]: !!currentNft },
         ])}
       >
-        <BorrowHeader
-          // onBackBtnClick={onBackBtnClick}
-          title="Borrow SOL"
-          subtitle={
-            isBulk
-              ? `${cartOrders.length + Number(!!currentNft)} loans in bulk`
-              : 'Select NFTs to borrow against'
-          }
-        />
         <Filters
           setSearch={setSearchDebounced}
           onSortChange={({ name, order }) => {
@@ -102,4 +219,28 @@ export const BorrowManualLitePage: FC = () => {
       </div>
     </AppLayout>
   );
+};
+
+type UseMaxBorrowValue = (props: { walletPublicKey?: web3.PublicKey }) => {
+  maxBorrowValue: number;
+  isLoading: boolean;
+};
+const useMaxBorrowValue: UseMaxBorrowValue = ({ walletPublicKey }) => {
+  const { data, isLoading } = useQuery(
+    ['maxBorrowValue', walletPublicKey?.toBase58()],
+    () =>
+      fetchMaxBorrowValue({
+        publicKey: walletPublicKey,
+      }),
+    {
+      enabled: !!walletPublicKey,
+      staleTime: 5 * 60 * 1000,
+      refetchOnWindowFocus: false,
+    },
+  );
+
+  return {
+    maxBorrowValue: data || 0,
+    isLoading,
+  };
 };
--- src/pages/BorrowPages/BorrowManualLitePage/components/BulkForm/BulkForm.tsx
+++ src/pages/BorrowPages/BorrowManualLitePage/components/BulkForm/BulkForm.tsx
@@ -5,56 +5,54 @@ import { Slider } from '@frakt/components/Slider';
 import { SOL_TOKEN } from '@frakt//utils';
 
 import styles from './BulkForm.module.scss';
-import { useBulkForm } from './hooks';
 
-export const BulkForm: FC = () => {
-  const {
-    borrowValue,
-    borrowPercentValue,
-    onBorrowValueChange,
-    onBorrowPercentChange,
-    maxBorrowValue,
-    loading,
-    isNotEnoughBalanceError,
-    isWalletConnected,
-  } = useBulkForm();
+interface BulkFormProps {
+  borrowValue: string;
+  borrowPercentValue: number;
+  onBorrowValueChange: (nextValue: string) => void;
+  onBorrowPercentChange: (nextValue: number) => void;
+  maxBorrowValue: number;
+  isNotEnoughBalanceError?: boolean;
+}
 
+export const BulkForm: FC<BulkFormProps> = ({
+  borrowValue,
+  borrowPercentValue,
+  onBorrowValueChange,
+  onBorrowPercentChange,
+  maxBorrowValue,
+  isNotEnoughBalanceError = false,
+}) => {
   return (
-    <>
-      {isWalletConnected && !loading && (
-        <div className={styles.block}>
-          <div className={styles.blockContent}>
-            <TokenFieldWithBalance
-              className={styles.input}
-              value={borrowValue}
-              onValueChange={onBorrowValueChange}
-              currentToken={SOL_TOKEN}
-              label="Available to borrow:"
-              lpBalance={parseFloat(maxBorrowValue?.toFixed(2))}
-              showMaxButton
-              error={isNotEnoughBalanceError}
-              labelRight
-            />
-            <div className={styles.errors}>
-              {isNotEnoughBalanceError && <p>Not enough NFTs</p>}
-            </div>
+    <div className={styles.root}>
+      <TokenFieldWithBalance
+        className={styles.input}
+        value={borrowValue}
+        onValueChange={onBorrowValueChange}
+        currentToken={SOL_TOKEN}
+        label="Available to borrow:"
+        lpBalance={parseFloat(maxBorrowValue?.toFixed(2))}
+        showMaxButton
+        error={isNotEnoughBalanceError}
+        labelRight
+      />
+      <div className={styles.errors}>
+        {isNotEnoughBalanceError && <p>Not enough NFTs</p>}
+      </div>
 
-            <Slider
-              value={borrowPercentValue}
-              setValue={onBorrowPercentChange}
-              className={styles.slider}
-              marks={{
-                0: '0 %',
-                25: '25 %',
-                50: '50 %',
-                75: '75 %',
-                100: '100 %',
-              }}
-              withTooltip
-            />
-          </div>
-        </div>
-      )}
-    </>
+      <Slider
+        value={borrowPercentValue}
+        setValue={onBorrowPercentChange}
+        className={styles.slider}
+        marks={{
+          0: '0 %',
+          25: '25 %',
+          50: '50 %',
+          75: '75 %',
+          100: '100 %',
+        }}
+        withTooltip
+      />
+    </div>
   );
 };
