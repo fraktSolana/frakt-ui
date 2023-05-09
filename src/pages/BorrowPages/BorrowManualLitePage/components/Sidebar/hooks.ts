c4c3a3ca8df0010e4f39a4708cff729e3ba713a6
sablevsky <vladislav1145@gmail.com>
2023-05-09 12:51:49 +0300

    Use borrowBulk on borrow lite

--- src/pages/BorrowPages/BorrowManualLitePage/components/Sidebar/hooks.ts
+++ src/pages/BorrowPages/BorrowManualLitePage/components/Sidebar/hooks.ts
@@ -1,25 +1,12 @@
 import { useState } from 'react';
-import { useWallet, WalletContextState } from '@solana/wallet-adapter-react';
+import { useWallet } from '@solana/wallet-adapter-react';
 import { useHistory } from 'react-router-dom';
-import { web3 } from 'fbonds-core';
 
 import { PATHS } from '@frakt/constants';
-import {
-  showSolscanLinkNotification,
-  signAndSendAllTransactions,
-  signAndSendAllTransactionsInSequence,
-} from '@frakt/utils/transactions';
-import { makeCreateBondMultiOrdersTransaction } from '@frakt/utils/bonds';
 import { useConnection } from '@frakt/hooks';
-import { useBorrow } from '@frakt/pages/BorrowPages/cartState';
-import { Market } from '@frakt/api/bonds';
-import { notify } from '@frakt/utils';
-import { NotifyType } from '@frakt/utils/solanaUtils';
-import { captureSentryError } from '@frakt/utils/sentry';
-import { makeProposeTransaction } from '@frakt/utils/loans';
-import { LoanType } from '@frakt/api/loans';
-import { BondCartOrder, BorrowNft } from '@frakt/api/nft';
+import { CartOrder, useBorrow } from '@frakt/pages/BorrowPages/cartState';
 import { useLoadingModalState } from '@frakt/components/LoadingModal';
+import { borrowBulk } from '@frakt/pages/BorrowPages/helpers';
 
 export const useSidebar = () => {
   const {
@@ -30,22 +17,16 @@ export const useSidebar = () => {
     onRemoveNft,
     onNextNftSelect,
     currentBondOrderParams,
-    market,
     currentLoanType,
     currentLoanValue,
     clearCart,
     clearCurrentNftState,
+    cartOrders,
   } = useBorrow();
 
   const [minimizedOnMobile, setMinimizedOnMobile] = useState<boolean>(false);
 
   const history = useHistory();
-
-  const goToToBorrowSuccessPage = () => {
-    clearCart();
-    clearCurrentNftState();
-    history.push(PATHS.BORROW_SUCCESS);
-  };
 
   const connection = useConnection();
   const wallet = useWallet();
@@ -58,23 +39,32 @@ export const useSidebar = () => {
 
   const onSubmit = async () => {
     try {
-      //TODO Logic txn here
+      //TODO closeConfirmModal();
 
       setLoadingModalVisible(true);
 
-      const result = await borrowSingle({
-        nft: currentNft,
-        bondOrderParams: currentBondOrderParams?.orderParams,
+      const currentOrder: CartOrder = {
+        borrowNft: currentNft,
         loanType: currentLoanType,
         loanValue: currentLoanValue,
-        market,
+        bondOrderParams: currentBondOrderParams,
+      };
+
+      const orders: CartOrder[] = [currentOrder, ...cartOrders];
+
+      const result = await borrowBulk({
+        orders,
         wallet,
         connection,
       });
+
       if (!result) {
         throw new Error('Borrow failed');
       }
-      goToToBorrowSuccessPage();
+
+      history.push(PATHS.BORROW_SUCCESS);
+      clearCurrentNftState();
+      clearCart();
     } catch (error) {
       console.error(error);
     } finally {
@@ -95,115 +85,4 @@ export const useSidebar = () => {
     loadingModalVisible,
     setLoadingModalVisible,
   };
-};
-
-type BorrowSingle = (props: {
-  nft: BorrowNft;
-  bondOrderParams?: BondCartOrder[];
-  market?: Market;
-  loanType: LoanType;
-  loanValue: number;
-  connection: web3.Connection;
-  wallet: WalletContextState;
-}) => Promise<boolean>;
-
-const borrowSingle: BorrowSingle = async ({
-  nft,
-  bondOrderParams,
-  market,
-  loanType,
-  loanValue,
-  connection,
-  wallet,
-}) => {
-  if (loanType !== LoanType.BOND) {
-    const { transaction, signers } = await makeProposeTransaction({
-      nftMint: nft.mint,
-      valuation: nft.valuation,
-      loanValue: loanValue,
-      loanType: loanType,
-      connection,
-      wallet,
-    });
-    return await signAndSendAllTransactions({
-      transactionsAndSigners: [{ transaction, signers }],
-      connection,
-      wallet,
-      commitment: 'confirmed',
-      onAfterSend: () => {
-        notify({
-          message: 'Transaction sent!',
-          type: NotifyType.INFO,
-        });
-      },
-      onSuccess: () => {
-        notify({
-          message: 'Borrowed successfully!',
-          type: NotifyType.SUCCESS,
-        });
-      },
-      onError: (error) => {
-        // eslint-disable-next-line no-console
-        console.warn(error.logs?.join('\n'));
-        const isNotConfirmed = showSolscanLinkNotification(error);
-
-        if (!isNotConfirmed) {
-          notify({
-            message: 'The transaction just failed :( Give it another try',
-            type: NotifyType.ERROR,
-          });
-        }
-
-        captureSentryError({
-          error,
-          wallet,
-          transactionName: 'borrowSingleClassic',
-        });
-      },
-    });
-  }
-
-  const { createBondTxnAndSigners, sellingBondsTxnsAndSigners } =
-    await makeCreateBondMultiOrdersTransaction({
-      nftMint: nft.mint,
-      market,
-      bondOrderParams: bondOrderParams,
-      connection,
-      wallet,
-    });
-
-  return await signAndSendAllTransactionsInSequence({
-    txnsAndSigners: [[createBondTxnAndSigners], sellingBondsTxnsAndSigners],
-    connection,
-    wallet,
-    commitment: 'confirmed',
-    onAfterSend: () => {
-      notify({
-        message: 'Transactions sent!',
-        type: NotifyType.INFO,
-      });
-    },
-    onSuccess: () => {
-      notify({
-        message: 'Borrowed successfully!',
-        type: NotifyType.SUCCESS,
-      });
-    },
-    onError: (error) => {
-      // eslint-disable-next-line no-console
-      console.warn(error.logs?.join('\n'));
-      const isNotConfirmed = showSolscanLinkNotification(error);
-      if (!isNotConfirmed) {
-        notify({
-          message: 'The transaction just failed :( Give it another try',
-          type: NotifyType.ERROR,
-        });
-      }
-      captureSentryError({
-        error,
-        wallet,
-        transactionName: 'borrowSingleBond',
-      });
-    },
-  });
 };
