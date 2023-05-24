d219dfc61858fcb2b4fd44d2cb4bbee832dda856
sablevsky <vladislav1145@gmail.com>
2023-05-24 17:03:18 +0300

    Refactor providers, constants in config and env names

--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,57 @@
 //? Comment to trigger vercel env build 6
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import {
-  LedgerWalletAdapter,
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-  SlopeWalletAdapter,
-  GlowWalletAdapter,
-  CoinbaseWalletAdapter,
-  TorusWalletAdapter,
-  MathWalletAdapter,
-  SolletWalletAdapter,
-  ExodusWalletAdapter,
-  BackpackWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
-import { SentreWalletAdapter } from '@sentre/connector';
 import {
   ConnectionProvider,
   WalletProvider,
 } from '@solana/wallet-adapter-react';
-import { FC, useEffect, useState } from 'react';
+import { FC } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { Router } from '@frakt/router';
 import store from '@frakt/state/store';
-import { getRightEndpoint } from '@frakt/config';
 import { initSentry } from '@frakt/utils/sentry';
 import { initAmplitude } from '@frakt/utils/amplitude';
 import Confetti from '@frakt/components/Confetti';
 import { ErrorBoundary } from '@frakt/components/ErrorBoundary';
 import { NotificationModal } from '@frakt/components/NotificationModal';
+import { useBestWorkingRPC } from '@frakt/hooks';
+import { ENDPOINTS, WALLETS } from '@frakt/config';
 
 initSentry();
 initAmplitude();
-
-const wallets = [
-  new PhantomWalletAdapter(),
-  new SolflareWalletAdapter(),
-  new SlopeWalletAdapter(),
-  new GlowWalletAdapter(),
-  new LedgerWalletAdapter(),
-  new CoinbaseWalletAdapter(),
-  new TorusWalletAdapter(),
-  new MathWalletAdapter(),
-  new ExodusWalletAdapter(),
-  new SentreWalletAdapter(),
-  new BackpackWalletAdapter(),
-  new SolletWalletAdapter({ network: WalletAdapterNetwork.Mainnet }),
-];
-
 const queryClient = new QueryClient();
 
 const App: FC = () => {
-  const [endpoint, setEndpoint] = useState<string>(null);
-
-  useEffect(() => {
-    (async () => {
-      const endpoint = await getRightEndpoint();
-      setEndpoint(endpoint);
-    })();
-  }, []);
-
-  if (!endpoint) return <></>;
-
   return (
     <ErrorBoundary>
-      <ReduxProvider store={store}>
-        <ConnectionProvider endpoint={endpoint}>
-          <WalletProvider wallets={wallets} autoConnect>
-            <QueryClientProvider client={queryClient}>
-              <Router />
-              <Confetti />
-              <NotificationModal />
-            </QueryClientProvider>
-          </WalletProvider>
-        </ConnectionProvider>
-      </ReduxProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReduxProvider store={store}>
+          <SolanaConnectionWalletProvider>
+            <Router />
+            <Confetti />
+            <NotificationModal />
+          </SolanaConnectionWalletProvider>
+        </ReduxProvider>
+      </QueryClientProvider>
     </ErrorBoundary>
   );
 };
 
 export default App;
+
+const SolanaConnectionWalletProvider: FC = ({ children }) => {
+  const { endpoint, isLoading } = useBestWorkingRPC({
+    endpoints: ENDPOINTS,
+    logErrors: true,
+  });
+
+  if (isLoading) return <></>;
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={WALLETS} autoConnect>
+        {children}
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
