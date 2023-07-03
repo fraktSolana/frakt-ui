3b7d7317b9a317d7b9ac6e1d43d779f02f27225c
vadiszzz <vadim.spb.korobey@gmail.com>
2023-07-03 16:23:55 +0400

    private markets and sdk update

--- src/api/auctions/requests.ts
+++ src/api/auctions/requests.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import { RefinanceAuctionItem } from './types';
+import { IS_PRIVATE_MARKETS } from '@frakt/config';
 
 export const fetchRefinanceAuctions = async (): Promise<
   RefinanceAuctionItem[]
 > => {
   const { data } = await axios.get<RefinanceAuctionItem[]>(
-    `https://${process.env.BACKEND_DOMAIN}/bonds/refinance`,
+    `https://${process.env.BACKEND_DOMAIN}/bonds/refinance?isPrivate=${IS_PRIVATE_MARKETS}`,
   );
 
   return data ?? [];
