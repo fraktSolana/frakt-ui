03fd7d5e3b66bcd6e4d2fb33cca6cea4722b4055
Vladislav <60342317+iamsphere@users.noreply.github.com>
2023-07-07 10:58:40 +0300

    Development (#928)
    * DEVS-1505: Auctions improvements (#923)
    
    * refinance auction text updates
    
    * Add new auction endpoint
    
    * Add search select filter
    
    * Modify SearchSelect component
    
    * Add goToLiteLending after success refinance
    
    * Adaptive SeatchSelect component
    
    * Add SolanaNFTLink component
    
    * Add totalAuctions in header
    
    * Add solanaFM icon
    
    * Fill icon for search select
    
    * add calc apy
    
    * add apy to interes row
    
    * Add sorting
    
    * Fix adaptives for search select
    
    * Fix adaptives for search select
    
    * Change default sort
    
    * Move filter to fetch auction hook
    
    * fix filter
    
    * fix width for solana fm button
    
    * fix position popup
    
    * change decimalPlaces for refinance modal
    
    ---------
    
    Co-authored-by: Timikcool <timikcool@yandex.ru>
    Co-authored-by: Vladislav <24393100+sablevsky@users.noreply.github.com>
    
    * Feature/devs 1505 (#925)
    
    * refinance auction text updates
    
    * Add new auction endpoint
    
    * Add search select filter
    
    * Modify SearchSelect component
    
    * Add goToLiteLending after success refinance
    
    * Adaptive SeatchSelect component
    
    * Add SolanaNFTLink component
    
    * Add totalAuctions in header
    
    * Add solanaFM icon
    
    * Fill icon for search select
    
    * add calc apy
    
    * add apy to interes row
    
    * Add sorting
    
    * Fix adaptives for search select
    
    * Fix adaptives for search select
    
    * Change default sort
    
    * Move filter to fetch auction hook
    
    * fix filter
    
    * fix width for solana fm button
    
    * fix position popup
    
    * change decimalPlaces for refinance modal
    
    ---------
    
    Co-authored-by: Timikcool <timikcool@yandex.ru>
    Co-authored-by: Vladislav <24393100+sablevsky@users.noreply.github.com>
    
    * write new timer without solanatimestamp (#927)
    
    ---------
    
    Co-authored-by: Timikcool <timikcool@yandex.ru>
    Co-authored-by: Vladislav <24393100+sablevsky@users.noreply.github.com>

--- src/hooks/useCountdown.ts
+++ src/hooks/useCountdown.ts
@@ -1,6 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
 import moment from 'moment';
-import { useSolanaTimestamp } from './useSolanaTimestamp';
 
 export interface TimeLeft {
   days: string;
@@ -17,7 +16,7 @@ type UseCountdown = (endTime: number) => {
 export const useCountdown: UseCountdown = (endTime: number) => {
   const intervalIdRef = useRef<ReturnType<typeof setInterval>>(null);
   const [currentTime, setCurrentTime] = useState<number | null>(null);
-  const { solanaTimestamp } = useSolanaTimestamp();
+  const [momentum, setMomentum] = useState<number>(0);
 
   const formatDateUnit = (value: number): string => {
     return value < 10 ? `0${value}` : `${value}`;
@@ -25,20 +24,18 @@ export const useCountdown: UseCountdown = (endTime: number) => {
 
   const endTimeMoment = moment.unix(endTime);
   const timeDifference = moment.duration(
-    endTimeMoment.diff(moment.unix(currentTime)),
+    endTimeMoment.diff(moment.unix(currentTime + momentum)),
   );
 
   useEffect(() => {
-    if (solanaTimestamp) {
-      setCurrentTime(solanaTimestamp);
+    setCurrentTime(moment().unix());
 
-      intervalIdRef.current = setInterval(() => {
-        setCurrentTime((prev) => prev + 1);
-      }, 1000);
-    }
+    intervalIdRef.current = setInterval(() => {
+      setMomentum((prevMomentum) => prevMomentum + 1);
+    }, 1000);
 
     return () => clearInterval(intervalIdRef.current);
-  }, [solanaTimestamp]);
+  }, []);
 
   useEffect(() => {
     timeDifference.asSeconds() < 0 && clearInterval(intervalIdRef.current);
