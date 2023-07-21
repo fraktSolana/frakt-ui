9b49fdba5fb1f4de6aac7ff829916e9e97b3b6eb
iamsphere <ezekiel.9218@gmail.com>
2023-07-21 17:20:35 +0300

    Fix parseAuctionsInfo export

--- src/pages/AuctionsPage/components/BondAuctionCard/helpers.ts
+++ src/pages/AuctionsPage/components/BondAuctionCard/helpers.ts
@@ -1,11 +1,9 @@
 import { AuctionItem } from '@frakt/api/auctions';
 
-const parseAuctionsInfo = (auction: AuctionItem) => {
+export const parseAuctionsInfo = (auction: AuctionItem) => {
   const { bondParams } = auction;
 
   const floorPrice = (bondParams?.floorPrice / 1e9)?.toFixed(3);
 
   return { floorPrice };
 };
-
-export { parseAuctionsInfo };
